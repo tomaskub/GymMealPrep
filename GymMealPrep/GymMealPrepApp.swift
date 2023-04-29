02faab58447c9e347a1a43b47f0f218f088c1c99
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-04-29 14:20:12 +0200

    Initial Commit

---
+++ GymMealPrep/GymMealPrepApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymMealPrepApp.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 4/29/23.
+//
+
+import SwiftUI
+
+@main
+struct GymMealPrepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
