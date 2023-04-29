eddd8e0eb59b2dded3faeac2c6c4acc823a27717
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-04-29 19:36:22 +0200

    Recipe: - Add temp init for previews - Change instructions to [Instruction] type
    Assets:
    - Add sample image for prototyping

--- GymMealPrep/Model/Recipie.swift
+++ GymMealPrep/Model/Recipie.swift
@@ -9,6 +9,7 @@ import Foundation
 
 struct Recipie {
     var id: UUID
+    var name: String
     var servings: Int
     var timeCookingInMinutes: Int
     var timePreparingInMinutes: Int
@@ -16,11 +17,42 @@ struct Recipie {
     
     var ingredients: [Ingredient]
     
-    var instructions: String
+    var instructions: [Instruction]
     
     var imageData: Data?
     
     var tags: [Tag]
     
     var nutritionData: Nutrition
+    
+    init(id: UUID, name: String,servings: Int, timeCookingInMinutes: Int, timePreparingInMinutes: Int, timeWaitingInMinutes: Int, ingredients: [Ingredient], instructions: [Instruction], imageData: Data? = nil, tags: [Tag], nutritionData: Nutrition) {
+        self.id = id
+        self.name = name
+        self.servings = servings
+        self.timeCookingInMinutes = timeCookingInMinutes
+        self.timePreparingInMinutes = timePreparingInMinutes
+        self.timeWaitingInMinutes = timeWaitingInMinutes
+        self.ingredients = ingredients
+        self.instructions = instructions
+        self.imageData = imageData
+        self.tags = tags
+        self.nutritionData = nutritionData
+    }
+    
+    init() {
+        self.init(id: UUID(),
+                  name: "Cilantro Lime Chicken",
+                  servings: 4,
+                  timeCookingInMinutes: 30,
+                  timePreparingInMinutes: 15,
+                  timeWaitingInMinutes: 5,
+                  ingredients: [],
+                  instructions: [],
+                  tags: [Tag(id: UUID(), text: "Chicken"),
+                         Tag(id: UUID(), text: "Rice"),
+                         Tag(id: UUID(), text: "Cilantro"),
+                         Tag(id: UUID(), text: "Lunch"),
+                         Tag(id: UUID(), text: "Lime")],
+                  nutritionData: Nutrition(calories: 800, carb: 30, fat: 18, protein: 38))
+    }
 }
