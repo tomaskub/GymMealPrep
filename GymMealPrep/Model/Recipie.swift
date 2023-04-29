f2ecb7062e831671ec329ed483c06478d5e7cc70
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-04-29 21:10:57 +0200

    Recipe: - Add ingredients and instructions to sample data

--- GymMealPrep/Model/Recipie.swift
+++ GymMealPrep/Model/Recipie.swift
@@ -46,8 +46,15 @@ struct Recipie {
                   timeCookingInMinutes: 30,
                   timePreparingInMinutes: 15,
                   timeWaitingInMinutes: 5,
-                  ingredients: [],
-                  instructions: [],
+                  ingredients: [
+                  Ingredient(food: Food(name: "Chicken breast"), quantity: 2, unitOfMeasure: "each", nutritionData: Nutrition()),
+                  Ingredient(food: Food(name: "Olive oil"), quantity: 1, unitOfMeasure: "tablespoon", nutritionData: Nutrition()),
+                  Ingredient(food: Food(name: "Basmati rice"), quantity: 2, unitOfMeasure: "cups", nutritionData: Nutrition())],
+                  instructions: [
+                  Instruction(id: UUID(), step: 1, text: "Place the chicken in a ziploc bag together with the olive oil, garlic, cumin, salt, pepper, and juice of 1 lime. Allow it to marinate for 15-20 min over the counter."),
+                  
+                  Instruction(id: UUID(), step: 2, text: "Preheat a skillet over medium-high heat. Cook the chicken for 3-5 minutes per side, or until cooked through."),
+                  Instruction(id: UUID(), step: 3, text: "Set aside and allow it to sit for 10 minutes, then slice.")],
                   tags: [Tag(id: UUID(), text: "Chicken"),
                          Tag(id: UUID(), text: "Rice"),
                          Tag(id: UUID(), text: "Cilantro"),
