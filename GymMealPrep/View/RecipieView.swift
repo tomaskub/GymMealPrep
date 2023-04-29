3bc53f3da52546bc5ce58af116aace11df8d7f1c
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-04-29 21:50:26 +0200

    RecipeViewModel: - Add array with nutrition data for the recipe
    RecipeView:
    - Add initial view structure

--- GymMealPrep/View/RecipieView.swift
+++ GymMealPrep/View/RecipieView.swift
@@ -8,13 +8,157 @@
 import SwiftUI
 
 struct RecipieView: View {
+    
+    @StateObject var viewModel: RecipieViewModel
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            //Background layer
+            Color.gray
+                .ignoresSafeArea()
+            
+            //Content layer
+            VStack {
+                titleBanner
+                
+                tagScrollView
+                
+                HStack {
+                    servingSummary
+                    timeSummary
+                }
+                .frame(height: 100)
+                
+                nutritionSummary
+                
+                List {
+                    Section("Ingredients:") {
+                        ForEach(viewModel.recipie.ingredients, id: \.food.name) {
+                            i in
+                            HStack {
+                                Text(i.food.name)
+                                Spacer()
+                                Text(String(format: "%.2f",i.quantity))
+                                Text(i.unitOfMeasure)
+                            }
+                            
+                        }
+                    }
+                    Section("Instructions") {
+                        ForEach(viewModel.recipie.instructions) { instruction in
+                            HStack{
+                                Text("\(instruction.step)")
+                                    .padding(.trailing)
+                                Text(instruction.text)
+                            }
+                        }
+                    }
+                }
+                
+                Spacer()
+                
+            }
+            
+            
+        }
+        
+        
+        
+    }
+    
+    var nutritionSummary: some View {
+        VStack {
+            Text("Nutrition value:")
+                .font(.title2)
+                .fontWeight(.semibold)
+            HStack {
+                ForEach(viewModel.nutritionalData, id: \.self) { data in
+                Text(data)
+                    .multilineTextAlignment(.center)
+                    .frame(width: 80, height: 80)
+                    .background()
+                    .cornerRadius(10)
+                }
+            }
+        }
+    }
+    
+    
+    var servingSummary: some View {
+        VStack {
+            Text("Servings:")
+                .font(.title3)
+                .fontWeight(.semibold)
+            Text("\(viewModel.recipie.servings)")
+        }
+        .padding()
+        .frame(maxHeight: .infinity)
+        .background(.white)
+        .cornerRadius(10)
+    }
+    
+    var timeSummary: some View {
+        VStack(alignment: .leading) {
+            Text("Time:")
+                .font(.title3)
+                .fontWeight(.semibold)
+                .padding(.leading)
+            HStack {
+                ForEach(viewModel.timeSummaryData, id: \.0) { data in
+                    VStack {
+                        Text(data.0)
+                        Text(data.1)
+                    }
+                }
+            }
+        }
+        .padding()
+        .frame(maxHeight: .infinity)
+        .background(.white)
+        .cornerRadius(10)
+    }
+    var titleBanner: some View {
+        viewModel.image
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .frame(maxWidth: .infinity)
+            .frame(height: 250)
+            .clipped()
+            .overlay {
+                ZStack {
+                    
+                    LinearGradient(colors: [.black, .black.opacity(0.0)], startPoint: .bottom, endPoint: .center)
+                    
+                    Text(viewModel.recipie.name)
+                        .font(.largeTitle)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+//                        .padding(.leading)
+                        .offset(y: 105)
+                    
+                }
+            }
+//            .ignoresSafeArea()
+    }
+    var tagScrollView: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack {
+                ForEach(viewModel.recipie.tags) { tag in
+                    Text(tag.text)
+                        .padding(.horizontal)
+                        .padding(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 0))
+                        .background(
+                            Capsule()
+                            .foregroundColor(.white))
+                }
+            }
+        }
     }
 }
 
 struct RecipieView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipieView()
+        RecipieView(viewModel: RecipieViewModel(recipie: Recipie()))
     }
 }
--- GymMealPrep/ViewModel/RecipieViewModel.swift
+++ GymMealPrep/ViewModel/RecipieViewModel.swift
@@ -28,6 +28,16 @@ class RecipieViewModel: ObservableObject {
         ]
     }
     
+    var nutritionalData: [String] {
+        [
+            String(format: "%.0f", recipie.nutritionData.calories) + "\n Cal",
+            String(format: "%.0f", recipie.nutritionData.protein) + "g\n Protein",
+            String(format: "%.0f", recipie.nutritionData.fat) + "g\n Fat",
+            String(format: "%.0f", recipie.nutritionData.carb) + "g\n Carb",
+        ]
+    }
+    
+    
     init(recipie: Recipie) {
         self.recipie = recipie
     }
