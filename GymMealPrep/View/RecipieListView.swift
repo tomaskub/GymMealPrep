09db2091f8c25c347b60de091c6b65e1fdfc1f69
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-04-30 12:02:25 +0200

    RecipieListView: - Initial add

---
+++ GymMealPrep/View/RecipieListView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipieListView.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 4/30/23.
+//
+
+import SwiftUI
+
+struct RecipieListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipieListView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipieListView()
+    }
+}
