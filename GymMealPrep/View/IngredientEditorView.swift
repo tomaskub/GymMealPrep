380e6c8ef2e008021d4d8ef658439f6ab4ff08b8
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-05-04 11:16:50 +0200

    IngredientEditorView: - Create view structure

--- GymMealPrep/View/IngredientEditorView.swift
+++ GymMealPrep/View/IngredientEditorView.swift
@@ -9,34 +9,110 @@ import SwiftUI
 
 struct IngredientEditorView: View {
     
+    @Environment(\.dismiss) var dismiss
+    
+    @Binding var name: String
+    @Binding var quantity: Double
+    @Binding var unitOfMeasure: String
+    @Binding var nutrition: Nutrition
+    
+    
+    
     var body: some View {
-        //TODO: ADD INGREDIENT MODIFICATION HERE
-        VStack(alignment: .leading) {
-            Text("Add new ingredient")
+        
+        VStack {
+            Text("Adding new ingredient")
                 .font(.title)
-            Grid {
-                GridRow{
-                    Text("Name")
-                    TextField("Name", text: .constant("Ingredient"))
+                .padding(.bottom)
+            
+            Grid(alignment: .centerFirstTextBaseline) {
+                Group {
+                    Text("Ingredient:")
+                        .font(.title2)
+                    
+                    Divider()
+                        .background(.white)
+                    
+                    GridRow{
+                        Text("Name")
+                        TextField("Name", text: $name)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    GridRow {
+                        Text("Quantity")
+                        TextField("quantity", value: $quantity, format: .number)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    GridRow {
+                        Text("Measurement")
+                        TextField("Unit of measure", text: $unitOfMeasure)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                } // END OF INGREDIENT GROUP
+                
+                Group {
+                    Text("Nutrition:")
+                        .font(.title2)
+                    
+                    Divider()
+                        .background(.white)
+                    
+                    GridRow{
+                        Text("Calories")
+                        TextField("cal", value: $nutrition.calories, format: .number)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    
+                    GridRow {
+                        Text("Protein")
+                        TextField("protein", value: $nutrition.protein, format: .number)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    
+                    GridRow {
+                        Text("Fat")
+                        TextField("fat", value: $nutrition.fat, format: .number)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    GridRow {
+                        Text("Carbs")
+                        TextField("carbs", value: $nutrition.carb, format: .number)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    
+                    
+                } // END OF NUTRITION GROUP
+            }// END OF GRID
+        
+            .padding()
+            .background(
+                Color.gray.opacity(0.2))
+            .cornerRadius(10)
+            
+            
+            HStack {
+                
+                Button("Cancel") {
+                    dismiss()
                 }
-                GridRow {
-                    Text("Quantity")
-                    Rectangle()
-                        .frame(width: 200,height: 100)
+                .buttonStyle(.borderedProminent)
+                
+                
+                Button("Save") {
+                    dismiss()
                 }
-                GridRow {
-                    Rectangle()
-                        .frame(width: 200)
-                }
+                .buttonStyle(.borderedProminent)
+                
             }
             Spacer()
-        }
+            
+        }// END OF VSTACK
         .padding()
     }
 }
 
 struct IngredientEditorView_Previews: PreviewProvider {
     static var previews: some View {
-        IngredientEditorView()
+        IngredientEditorView(name: .constant("Chicken breast"), quantity: .constant(2), unitOfMeasure: .constant("each"), nutrition: .constant(Nutrition(calories: 600.5, carb: 8, fat: 10, protein: 30)))
     }
 }
