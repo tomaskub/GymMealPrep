f9754edab4f538e3f5370adaa6bc86169747c9bf
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-05-05 11:51:19 +0200

    - Add published array of recipes - Add NSFetchedResultsController for recipes - Implement FRC delegate methods - Add marks and comments

--- GymMealPrep/DataManager.swift
+++ GymMealPrep/DataManager.swift
@@ -13,15 +13,21 @@ enum DataManagerType {
 }
 
 class DataManager: NSObject, ObservableObject {
+    //MARK: PUBLISHED PROPERTIES
+    @Published var recipieArray = [Recipe]()
     
+    //MARK: STATIC INSTANCES
     static let shared = DataManager(type: .normal)
     static let testing = DataManager(type: .testing)
     static let preview = DataManager(type: .preview)
     
+    //MARK: PRIVATE PROPERTIES
     fileprivate var managedContext: NSManagedObjectContext
+    private let recipieFRC: NSFetchedResultsController<RecipieMO>
     
+    //MARK: INIT
     private init(type: DataManagerType) {
-        
+        //Set container depending on type
         switch type {
         case .normal:
             let persistanceContainer = PersistanceContainer()
@@ -29,7 +35,6 @@ class DataManager: NSObject, ObservableObject {
         case .testing:
             let persistanceContainer = PersistanceContainer(inMemory: true)
             self.managedContext = persistanceContainer.viewContext
-            
         case .preview:
             let persistanceContainer = PersistanceContainer(inMemory: true)
             self.managedContext = persistanceContainer.viewContext
@@ -37,6 +42,28 @@ class DataManager: NSObject, ObservableObject {
             try? self.managedContext.save()
             
         }
+        //Build FRC
+        let fetchRequest = RecipieMO.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "name", ascending: true)]
+        self.recipieFRC = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: managedContext, sectionNameKeyPath: nil, cacheName: nil)
+        
         super.init()
+        //Initial fetch
+        try? recipieFRC.performFetch()
+        if let newRecipes = recipieFRC.fetchedObjects {
+            self.recipieArray = newRecipes.map { Recipe(recipieMO: $0) }
+        }
     }
 }
+
+//MARK: FRC DELEGATE METHODS
+extension DataManager: NSFetchedResultsControllerDelegate {
+    
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<NSFetchRequestResult>) {
+            guard let fetchedObjects = controller.fetchedObjects else { return }
+            let newRecipes = fetchedObjects.compactMap({$0 as? RecipieMO})
+            self.recipieArray = newRecipes.map({ Recipe(recipieMO: $0) })
+        
+    }
+}
+
