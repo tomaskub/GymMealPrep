08b8a22a7c2323825b2d12e11447a03986a33213
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-05-06 17:07:52 +0200

    FoodMO: - Add public init with all data - Restrict use of inits without all the data initialized

--- GymMealPrep/CoreDataModel/FoodMO+CoreDataClass.swift
+++ GymMealPrep/CoreDataModel/FoodMO+CoreDataClass.swift
@@ -11,5 +11,26 @@ import CoreData
 
 @objc(FoodMO)
 public class FoodMO: NSManagedObject {
-
+    
+    public init(context: NSManagedObjectContext, id: UUID = UUID(), name: String) {
+        let entityDescription = NSEntityDescription.entity(forEntityName: "FoodMO", in: context)!
+        super.init(entity: entityDescription, insertInto: context)
+        self.id = id
+        self.name = name
+    }
+    
+    @objc override private init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertInto: context)
+    }
+    
+    @available(*, unavailable)
+    public init() {
+        fatalError("\(#function) not implemented")
+    }
+    
+    @available(*, unavailable)
+    public convenience init(context: NSManagedObjectContext) {
+        fatalError("\(#function) not implemented")
+    }
+    
 }
