4827d5b595e534718a75d0e7a2230ac7fb26e8dd
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-05-06 17:17:37 +0200

    Add init with properties Restrict inits without properties initialized

--- GymMealPrep/CoreDataModel/TagMO+CoreDataClass.swift
+++ GymMealPrep/CoreDataModel/TagMO+CoreDataClass.swift
@@ -12,4 +12,23 @@ import CoreData
 @objc(TagMO)
 public class TagMO: NSManagedObject {
 
+    public init(context: NSManagedObjectContext, id: UUID = UUID(), text: String) {
+        let entityDescription = NSEntityDescription.entity(forEntityName: "TagMO", in: context)!
+        super.init(entity: entityDescription, insertInto: context)
+        self.id = id
+        self.text = text
+    }
+    
+    @objc override private init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertInto: context)
+    }
+    
+    @available(*, unavailable)
+    public init() {
+        fatalError("\(#function) not implemented")
+    }
+    @available(*, unavailable)
+    public convenience init(context: NSManagedObjectContext){
+        fatalError("\(#function) not implemented")
+    }
 }
