a962331ef197f04a30c9dfda7962803712ccc0dd
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-05-16 20:59:03 +0200

    Add NetworkController class conforming to Network Protocol

---
+++ GymMealPrep/NetworkManager/NetworkController.swift
@@ -0,0 +1,37 @@
+//
+//  NetworkController.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 5/16/23.
+//
+
+import Foundation
+import Combine
+
+/// Concrete implementation of network controller protocol
+final class NetworkController: Network {
+    
+    func get<T>(type: T.Type, url: URL, headers: Headers) -> AnyPublisher<T, Error> where T : Decodable {
+        var urlRequest = URLRequest(url: url)
+        
+        headers.forEach { (key, value) in
+            if let value = value as? String {
+                urlRequest.setValue(value, forHTTPHeaderField: key)
+            }
+        }
+        //TODO: HANDLE URL RESPONSE TO ERRORS BETTER
+        return URLSession.shared.dataTaskPublisher(for: urlRequest)
+            .tryMap { (data, response) -> Data in
+                guard let response = response as? HTTPURLResponse,
+                      response.statusCode >= 200 && response.statusCode < 300 else {
+                    throw URLError(.badServerResponse)
+                }
+                return data
+            }
+            .decode(type: T.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+    
+    
+}
+
