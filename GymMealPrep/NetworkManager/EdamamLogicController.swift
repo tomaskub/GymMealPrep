e3ab77d6407a5fce4c1d0569df0c282c741f7a3f
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-05-19 22:07:39 +0200

    EdamamLogicController: - Add func transformParserResponse to organize hint objects into arrays of Ingredients -  Change map in getIngredients publisher to call new func

--- GymMealPrep/NetworkManager/EdamamLogicController.swift
+++ GymMealPrep/NetworkManager/EdamamLogicController.swift
@@ -29,33 +29,69 @@ final class EdamamLogicController: EdamamLogicControllerProtocol {
         return networkController.get(type: EdamamParserResponse.self, url: endpoint.url, headers: [:])
     }
     
-    func getIngredients(for ingredient: String) -> AnyPublisher<[Ingredient], Error> {
+    func getIngredients(for ingredient: String) -> AnyPublisher<[[Ingredient]], Error> {
         let endpoint = EdamamParserEndpoint.ingredient(searchFor: ingredient)
         return networkController.get(type: EdamamParserResponse.self, url: endpoint.url, headers: [:])
-            .map { response in
-                var mapResult = [Ingredient]()
-                for hint in response.hints {
-                    let food = Food(name: hint.food.label)
-                    let nutrients = Nutrition(fromEdamam: hint.food.nutrients)
-                    
-                    let ingredient = Ingredient(
-                        food: food,
-                        quantity: 1,
-                        unitOfMeasure: hint.measures.first?.label ?? "Unknown",
-                        nutritionData: nutrients
-                    )
-                    mapResult.append(ingredient)
-                }
-                return mapResult
+            //TODO: EXTRACT MAP TO SEPERATE FUNCTION
+            .map { [weak self] response in
+                
+                guard let self else { return [[]] }
+                let result  = self.transformParserResponse(response.hints)
+                return result
             }
             .eraseToAnyPublisher()
         
+    }
+    
+    /// Transform hints from edamam parser response hints to an array of array of ingredients
+    /// - Parameter responseHints: Hint array retrived from Edamam Parser response
+    /// - Returns: An array of array of ingredients
+    private func transformParserResponse(_ responseHints: [EdamamParserResponse.Hint]) -> [[Ingredient]] {
+        
+        var mapResult = [[Ingredient]]()
+        
+        for hint in responseHints {
+            
+            var foodResult = [Ingredient]()
+            
+            let food = Food(name: hint.food.label)
+            let nutrients = Nutrition(fromEdamam: hint.food.nutrients)
+
+            for measure in hint.measures {
+                
+                if let qualifiedMeasures = measure.qualified {
+                    for qualifier in qualifiedMeasures {
+                        let factor = qualifier.weight / 100 // standard nutrition is given for 100g
+                        let nutritionData = nutrients.multiplyBy(factor)
+                        let uom = measure.label + " (" + qualifier.qualifiers.first!.label + ")"
+                        let ingredient = Ingredient(food: food, quantity: 1, unitOfMeasure: uom, nutritionData: nutritionData)
+                        foodResult.append(ingredient)
+                    }
+                } else {
+                      // measure has no qualifiers  create
+                    let factor = measure.weight / 100 // standard nutrition is given for 100g
+                    let nutritionData = nutrients.multiplyBy(factor)
+                    let ingredient = Ingredient(
+                        food: food,
+                        quantity: 1,
+                        unitOfMeasure: measure.label,
+                        nutritionData: nutritionData
+                    )
+                    foodResult.append(ingredient)
+                }
+            }
+            mapResult.append(foodResult)
+        }
+        return mapResult
     }
     
 }
 
 fileprivate extension Nutrition {
     init(fromEdamam nutrient: EdamamParserResponse.Nutrients) {
-        self.init(calories: Float(nutrient.enercKcal), carb: Float(nutrient.chocdf), fat: Float(nutrient.fat), protein: Float(nutrient.procnt))
+        self.init(calories: Float(nutrient.enercKcal ?? 0),
+                  carb: Float(nutrient.carbs ?? 0),
+                  fat: Float(nutrient.fat ?? 0),
+                  protein: Float(nutrient.protein ?? 0))
     }
 }
