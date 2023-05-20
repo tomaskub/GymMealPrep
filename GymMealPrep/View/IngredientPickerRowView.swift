c38506f9573dbcd0c52a55aa9ed3dfbd7baa0faf
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-05-20 10:04:19 +0200

    IngredientPickerRowView - initial add

---
+++ GymMealPrep/View/IngredientPickerRowView.swift
@@ -0,0 +1,20 @@
+//
+//  IngredientPickerRowView.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 5/20/23.
+//
+
+import SwiftUI
+
+struct IngredientPickerRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IngredientPickerRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        IngredientPickerRowView()
+    }
+}
