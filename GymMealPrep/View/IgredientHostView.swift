360798760c35b44f237ba11ce9a83f0786ee3185
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-05-22 20:57:28 +0200

    IngredientHostView: - Initial add

---
+++ GymMealPrep/View/IgredientHostView.swift
@@ -0,0 +1,20 @@
+//
+//  IgredientHostView.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 5/22/23.
+//
+
+import SwiftUI
+
+struct IgredientHostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IgredientHostView_Previews: PreviewProvider {
+    static var previews: some View {
+        IgredientHostView()
+    }
+}
