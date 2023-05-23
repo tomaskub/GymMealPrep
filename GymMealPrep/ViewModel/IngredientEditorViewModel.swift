536aa1b4a83b824bcdadc1590848fcff251f0c67
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-05-23 22:16:33 +0200

    IngredientEditorViewModel: - Add properties for view state and data entry - Add initi for the class

--- GymMealPrep/ViewModel/IngredientEditorViewModel.swift
+++ GymMealPrep/ViewModel/IngredientEditorViewModel.swift
@@ -9,4 +9,40 @@ import Foundation
 
 class IngredientEditorViewModel: ObservableObject {
     
+    @Published var lockNutritionValues: Bool
+    @Published var isEditingIngredient: Bool
+    @Published var draftIngredient: Ingredient
+    
+    
+    
+    //MARK: View textFields properties
+    //Initialized here so they can be overriden in init using draftIngredient values
+    @Published var ingredientName = String()
+    @Published var ingredientQuantity = String()
+    @Published var ingredientUnitOfMeasure = String()
+    @Published var ingredientCalories = String()
+    @Published var ingredientProtein = String()
+    @Published var ingredientFat = String()
+    @Published var ingredientCarbs = String()
+    
+    init(ingredientToEdit: Ingredient?) {
+        //unwrap and configure view state based on wheter the ingredient was passed in
+        if let ingredient = ingredientToEdit {
+            self.draftIngredient = ingredient
+            self.lockNutritionValues = true
+            self.isEditingIngredient = true
+        } else {
+            self.draftIngredient = Ingredient()
+            self.lockNutritionValues = false
+            self.isEditingIngredient = false
+        }
+        // override field texts properties
+        self.ingredientName = draftIngredient.food.name
+        self.ingredientQuantity = String(draftIngredient.quantity)
+        self.ingredientUnitOfMeasure = draftIngredient.unitOfMeasure
+        self.ingredientCalories = String(draftIngredient.nutritionData.calories)
+        self.ingredientProtein = String(draftIngredient.nutritionData.protein)
+        self.ingredientFat = String(draftIngredient.nutritionData.fat)
+        self.ingredientCarbs = String(draftIngredient.nutritionData.carb)
+    }
 }
