f541a525961004578db60350b31d01fa1975279b
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-05-23 21:47:26 +0200

    Initial add of IngredientEditorViewModel

---
+++ GymMealPrep/ViewModel/IngredientEditorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  IngredientEditorViewModel.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 5/23/23.
+//
+
+import Foundation
+
+class IngredientEditorViewModel: ObservableObject {
+    
+}
