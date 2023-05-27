daccee59785e612fccdf5b24ecc76e62419b20bd
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-05-27 19:13:54 +0200

    Add content to recipe creator view

--- GymMealPrep/View/RecipeViews/RecipeCreatorView.swift
+++ GymMealPrep/View/RecipeViews/RecipeCreatorView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct RecipeCreatorView: View {
+    
+    @State private var ingredients = String()
+    @State private var instructions = String()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading) {
+            
+            Text("Ingredients:")
+                .fontWeight(.semibold)
+                .font(.title3)
+            
+            TextEditor(text: $ingredients)
+                .scrollContentBackground(.hidden)
+                .background(.gray.opacity(0.1))
+                .cornerRadius(20)
+            
+            Text("Instructions:")
+                .fontWeight(.semibold)
+                .font(.title3)
+            
+            TextEditor(text: $instructions)
+                .scrollContentBackground(.hidden)
+                .background(.gray.opacity(0.1))
+                .cornerRadius(20)
+            
+            HStack {
+                Spacer()
+                Button {
+                    print("Create button pressed")
+                    print(ingredients)
+                    print(instructions)
+                } label: {
+                    Text("Creating recipe")
+                }
+                .buttonStyle(.borderedProminent)
+            }
+        }
+        .padding()
+        .navigationTitle("Create recipe from text")
+        
     }
 }
 
 struct RecipeCreatorView_Previews: PreviewProvider {
     static var previews: some View {
-        RecipeCreatorView()
+        NavigationView {
+            RecipeCreatorView()
+        }
     }
 }
