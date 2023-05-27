5f3d35f1cc973b03443b4ee973834c88693202d2
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-05-27 19:17:29 +0200

    RecipeCreatorViewModel initial add

---
+++ GymMealPrep/ViewModel/RecipeCreatorViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  RecipeCreatorViewModel.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 5/27/23.
+//
+
+import Foundation
+
+class RecipeCreatorViewModel: ObservableObject {
+    
+}
