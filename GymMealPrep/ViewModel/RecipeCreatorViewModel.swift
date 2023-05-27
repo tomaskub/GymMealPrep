4000f5d87c46c2225ccea0fda4f2e846ed724478
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-05-27 21:22:59 +0200

    - Add published input properties for view - Add published parsed properties - Add network calls to the parse the input data

--- GymMealPrep/ViewModel/RecipeCreatorViewModel.swift
+++ GymMealPrep/ViewModel/RecipeCreatorViewModel.swift
@@ -6,7 +6,38 @@
 //
 
 import Foundation
+import Combine
 
 class RecipeCreatorViewModel: ObservableObject {
     
+    @Published var ingredientsEntry: String
+    @Published var instructionsEntry: String
+    @Published var parsed: [EdamamParserResponse]
+    @Published var ingredientsNLArray: [String]
+    
+    var subscriptions = Set<AnyCancellable>()
+    let edamamLogicController: EdamamLogicControllerProtocol = EdamamLogicController(networkController: NetworkController())
+    
+    init() {
+        self.ingredientsEntry = String()
+        self.instructionsEntry = String()
+        self.ingredientsNLArray = [String]()
+        self.parsed = [EdamamParserResponse]()
+    }
+    
+    func processInput() {
+        // create natural language array of ingredients to use with edamam parser
+        ingredientsNLArray = ingredientsEntry.components(separatedBy: .newlines)
+        for searchTerm in ingredientsNLArray {
+            edamamLogicController.getIngredients(for: searchTerm)
+                .receive(on: DispatchQueue.main)
+                .sink { completion in
+                    print(completion)
+                } receiveValue: { [weak self] response in
+                    guard let self else { return }
+                    self.parsed.append(response)
+                }
+                .store(in: &subscriptions)
+        }
+    }
 }
