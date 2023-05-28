e0d9149f3bd12899b146a07c4795d45ef290c380
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-05-28 10:43:56 +0200

    Add viewModel and build initial view Add preview view model implementation with static data to avoid calls to api

--- GymMealPrep/View/RecipeViews/RecipeCreatorParserView.swift
+++ GymMealPrep/View/RecipeViews/RecipeCreatorParserView.swift
@@ -8,13 +8,44 @@
 import SwiftUI
 
 struct RecipeCreatorParserView: View {
+    
+    @ObservedObject var viewModel: RecipeCreatorViewModelProtocol
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        VStack {
+            
+            Text("Ingredients:")
+                .font(.title)
+            
+            ForEach(viewModel.parsedIngredients, id: \.self.0) { response in
+                // TODO: IMPLEMENT UI
+                Text(response.1.first?.food.name ?? "Error")
+            } // END OF FOR EACH
+        } // END OF VSTACK
+        .onAppear {
+            viewModel.processInput()
+        } // END OF ON APPEAR
+    } // END OF BODY
+} // END OF STRUCT
 
 struct RecipeCreatorParserView_Previews: PreviewProvider {
+    
+    class PreviewViewModel: RecipeCreatorViewModelProtocol {
+        
+        override init() {
+            super.init()
+            self.parsedIngredients = [
+                ("1 cup of rice", [Ingredient(food: Food(name: "Rice"), quantity: 1, unitOfMeasure: "Cup", nutritionData: Nutrition.zero)]),
+            ("1 chicken breast", [Ingredient(food: Food(name: "Chicken breast"), quantity: 1, unitOfMeasure: "Piece", nutritionData: Nutrition.zero)]),
+                ("2 cups of broccoli florets", [Ingredient(food: Food(name: "Broccoli"), quantity: 2, unitOfMeasure: "Cup", nutritionData: Nutrition.zero)])
+            ]
+        } // END OF INIT
+        
+        override func processInput() {
+            print("Processing input call")
+        }
+    } // END OF CLASS
     static var previews: some View {
-        RecipeCreatorParserView()
+        RecipeCreatorParserView(viewModel: PreviewViewModel())
     }
 }
