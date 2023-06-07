1a7f98e0fc348b2d573d25a85b21458e9953ca36
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-07 21:04:37 +0200

    Initial add of InstructionRowView

---
+++ GymMealPrep/View/SubViews/InstructionRowView.swift
@@ -0,0 +1,20 @@
+//
+//  InstructionRowView.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 6/7/23.
+//
+
+import SwiftUI
+
+struct InstructionRowView: View {
+    var body: some View {
+        Text("Hello world!")
+    }
+}
+
+struct InstructionRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        InstructionRowView()
+    }
+}
