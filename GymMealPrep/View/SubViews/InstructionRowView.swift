f0f8c5b6441ddb5632aa5d063b8640dd78b41c38
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-07 21:25:26 +0200

    Implement variables, view body and previews

--- GymMealPrep/View/SubViews/InstructionRowView.swift
+++ GymMealPrep/View/SubViews/InstructionRowView.swift
@@ -8,13 +8,38 @@
 import SwiftUI
 
 struct InstructionRowView: View {
+    
+    @Binding var instructionText: String
+    let step: Int
+    let editable: Bool
+    
+    
     var body: some View {
-        Text("Hello world!")
+        HStack(alignment: .top, spacing: 20) {
+            
+            Text("\(step)")
+                .fontWeight(.semibold)
+                .padding(.top, 8)
+            
+            // use text to size the text editor through zstack
+            ZStack {
+                Text(instructionText)
+                    .opacity(editable ? 0 : 1)
+                
+                if editable {
+                    TextEditor(text: $instructionText)
+                }
+            }
+        }
     }
 }
 
 struct InstructionRowView_Previews: PreviewProvider {
     static var previews: some View {
-        InstructionRowView()
+        List {
+            ForEach(1..<5) { step in
+                InstructionRowView(instructionText: .constant("This is a very long text that has multiple lines and therefore has to make sure that it is in the length that forces the view height key to trigger the redraw"), step: step, editable: step.isMultiple(of: 2))
+            }
+        }
     }
 }
