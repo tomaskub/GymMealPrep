8cbf2f0ea738e370510276a04bf7958913fdc5f7
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-08 10:25:31 +0200

    Initial add of NutritionStripeView

---
+++ GymMealPrep/View/IngredientViews/NutritionStripeView.swift
@@ -0,0 +1,21 @@
+//
+//  NutritionStripeView.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 6/8/23.
+//
+
+import SwiftUI
+
+struct NutritionStripeView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NutritionStripeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NutritionStripeView()
+    }
+}
