3d5649032a5b6086d623d8d722416df10f54f34a
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-08 22:44:28 +0200

    Add number formatting for nutrition data displayed

--- GymMealPrep/View/IngredientViews/NutritionStripeView.swift
+++ GymMealPrep/View/IngredientViews/NutritionStripeView.swift
@@ -11,27 +11,40 @@ struct NutritionStripeView: View {
     
     let nutrition: any NutritionProtocol
     
+    let formatter: NumberFormatter = {
+       let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 0
+        return formatter
+    }()
+    
     var body: some View {
         HStack {
             Text("Calories")
                 .foregroundColor(.gray)
-            Text(String(nutrition.calories))
+            Text(formatedValue(nutrition.calories))
             Text("Fat")
                 .foregroundColor(.gray)
-            Text(String(nutrition.fat))
+            Text(formatedValue(nutrition.fat))
             Text("Carbs")
                 .foregroundColor(.gray)
-            Text(String(nutrition.carb))
+            Text(formatedValue(nutrition.carb))
             Text("Protein")
                 .foregroundColor(.gray)
-            Text(String(nutrition.protein))
-        }
+            Text(formatedValue(nutrition.protein))
+            
+        } // END OF H STACK
         .font(.caption)
+    } // END OF BODY
+    
+    private func formatedValue(_ value: Float) -> String {
+        formatter.string(from: NSNumber(value: value)) ?? "0.0"
     }
-}
+} // END OF STRUCT
 
 struct NutritionStripeView_Previews: PreviewProvider {
     static var previews: some View {
-        NutritionStripeView(nutrition: Nutrition(calories: 1000, carb: 45.3, fat: 20, protein: 34.99))
+        NutritionStripeView(nutrition: Nutrition(calories: 1000, carb: 45.3, fat: 20, protein: 34.00000999))
     }
 }
