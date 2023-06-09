998213bc6fe9ece15018ad2eda754fbc80031982
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-09 15:05:31 +0200

    Add ViewWidthKey conformance to PreferenceKey

--- GymMealPrep/Helpers/ViewWidthKey.swift
+++ GymMealPrep/Helpers/ViewWidthKey.swift
@@ -5,4 +5,11 @@
 //  Created by Tomasz Kubiak on 6/9/23.
 //
 
-import Foundation
+import SwiftUI
+
+struct ViewWidthKey: PreferenceKey {
+    static var defaultValue: CGFloat { 0 }
+    static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {
+        value = nextValue()
+    }
+}
