fbd81021a4b4f9cf39ee76b230d36f15cb895f6a
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-10 22:49:00 +0200

    Initial add of ExpandableButtonPanel

---
+++ GymMealPrep/View/SubViews/ExpandableButtonPanel.swift
@@ -0,0 +1,20 @@
+//
+//  ExpandableButtonPanel.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 6/10/23.
+//
+
+import SwiftUI
+
+struct ExpandableButtonPanel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ExpandableButtonPanel_Previews: PreviewProvider {
+    static var previews: some View {
+        ExpandableButtonPanel()
+    }
+}
