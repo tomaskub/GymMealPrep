c906e0525509dd2211a3fd2dd22d1c716372a0a3
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-11 11:30:49 +0200

    Add initial UI to expandable button panel

--- GymMealPrep/View/SubViews/ExpandableButtonPanel.swift
+++ GymMealPrep/View/SubViews/ExpandableButtonPanel.swift
@@ -8,13 +8,55 @@
 import SwiftUI
 
 struct ExpandableButtonPanel: View {
+    
+    let primaryButton: ExpandableButtonItem
+    let secondaryButtons: [ExpandableButtonItem]
+    
+    private let size: CGFloat = 70
+    @State private var isExpanded: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Button("<") {
+                withAnimation {
+                    self.isExpanded.toggle()
+                }
+            }
+            ForEach(secondaryButtons) { button in
+                Text(button.label)
+                    .onTapGesture {
+                        if let action = button.action {
+                            action()
+                        }
+                    }
+                    .frame(
+                        width: self.isExpanded ? self.size : 0,
+                        height: self.isExpanded ? self.size : 0
+                    )
+            }
+            Button(primaryButton.label) {
+                if let action = primaryButton.action {
+                    action()
+                }
+            }
+        }
     }
+}
+
+struct ExpandableButtonItem: Identifiable {
+    let id = UUID()
+    let label: String
+    private(set) var action: (()-> Void)? = nil
 }
 
 struct ExpandableButtonPanel_Previews: PreviewProvider {
     static var previews: some View {
-        ExpandableButtonPanel()
+        ExpandableButtonPanel(
+            primaryButton: ExpandableButtonItem(label: "P"),
+            secondaryButtons: [
+                ExpandableButtonItem(label: "S"),
+            ExpandableButtonItem(label: "T")
+            ]
+        )
     }
 }
