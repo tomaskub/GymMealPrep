7468f1fb9ed6f174ef0e7f78cac878ef3c8c5f9a
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-12 22:09:13 +0200

    Initial add of RecipeListTabView

---
+++ GymMealPrep/View/TabViews/RecipeListTabView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeListTabView.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 6/12/23.
+//
+
+import SwiftUI
+
+struct RecipeListTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeListTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeListTabView()
+    }
+}
