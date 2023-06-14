ceb1bc230c71f2f7c5f7abc39949cbc821ba1ede
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-14 14:29:49 +0200

    Initial add of RecipeCreatorConfirmationView and HomeTabView

---
+++ GymMealPrep/View/RecipeViews/RecipeCreatorConfirmationView.swift
@@ -0,0 +1,21 @@
+//
+//  RecipeCreatorConfirmationView.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 6/14/23.
+//
+
+import SwiftUI
+
+struct RecipeCreatorConfirmationView: View {
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeCreatorConfirmationView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeCreatorConfirmationView()
+    }
+}
---
+++ GymMealPrep/View/TabViews/HomeTabView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTabView.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 6/14/23.
+//
+
+import SwiftUI
+
+struct HomeTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeTabView()
+    }
+}
