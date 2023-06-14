82a1264866eecda0404f7b4abd14f2bf673407b2
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-14 14:33:56 +0200

    Initial add of RecipeCreatorHostView

---
+++ GymMealPrep/View/RecipeViews/RecipeCreatorHostView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipeCreatorHostView.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 6/14/23.
+//
+
+import SwiftUI
+
+struct RecipeCreatorHostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipeCreatorHostView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeCreatorHostView()
+    }
+}
