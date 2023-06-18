9392946130fc1c7f98359161371a466feadad8bf
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-18 13:56:47 +0200

    Add floating tooltip for the instruction input

--- GymMealPrep/View/RecipeViews/RecipeCreatorView.swift
+++ GymMealPrep/View/RecipeViews/RecipeCreatorView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct RecipeCreatorView: View {
     
     @ObservedObject var viewModel: RecipeCreatorViewModelProtocol
-    
+    @State var isShowingInstructionTooltip: Bool = false
     var body: some View {
         VStack(alignment: .leading) {
             
@@ -36,15 +36,49 @@ struct RecipeCreatorView: View {
                 .background(.gray.opacity(0.1))
                 .cornerRadius(20)
             
-            Text("Instructions:")
-                .fontWeight(.semibold)
-                .font(.title3)
+            HStack {
+                
+                Text("Instructions:")
+                    .fontWeight(.semibold)
+                    .font(.title3)
+                    
+                Image(systemName: "info")
+                    .padding(.all, 5)
+                    .background(
+                    Circle()
+                        .foregroundColor(.gray.opacity(0.2)))
+                    .onTapGesture {
+                        withAnimation {
+                            isShowingInstructionTooltip.toggle()
+                        }
+                    }
+            } // END OF HSTACK
             
             TextEditor(text: $viewModel.instructionsEntry)
                 .scrollContentBackground(.hidden)
                 .padding(EdgeInsets(top: 4, leading: 4, bottom: 4, trailing: 4))
                 .background(.gray.opacity(0.1))
                 .cornerRadius(20)
+                .overlay {
+                    if isShowingInstructionTooltip {
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("Tip:")
+                                .fontWeight(.semibold)
+                            Text("Use numbers to start new instruction to make sure the translation is sucessful.")
+                                .multilineTextAlignment(.leading)
+                            //                            .padding(.horizontal)
+                            Text("Example:")
+                                .fontWeight(.semibold)
+                            Text("1. First sample instruction.\n2. Second dample instruction")
+                        } // END OF VSTACK
+                        .padding()
+                        .background(
+                            Color.white
+                        )
+                        .cornerRadius(20)
+                        .transition(.opacity)
+                    } // END OF IF CONDITION
+                } // END OF OVERLAY
         } // END OF VSTACK
         .padding()
         .navigationTitle("Create recipe")
