8d25f933f62a1494758a2b6d409fd4492d02efb9
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-19 21:09:18 +0200

    MealPlanTabView initial add

---
+++ GymMealPrep/View/TabViews/MealPlanTabView.swift
@@ -0,0 +1,20 @@
+//
+//  MealPlanTabView.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 6/19/23.
+//
+
+import SwiftUI
+
+struct MealPlanTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MealPlanTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MealPlanTabView()
+    }
+}
