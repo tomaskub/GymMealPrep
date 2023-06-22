5bf7f16b17b5470c7eb878fe97fa65b923a28694
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-22 14:48:39 +0200

    Add recipe breakfast potatoe hash to sample data

--- GymMealPrep/SampleData.swift
+++ GymMealPrep/SampleData.swift
@@ -35,6 +35,33 @@ struct SampleData {
                Tag(id: UUID(), text: "Lime"),
               Tag(text: "Dinner")])
     
+    static let recipeBreakfastPotatoHash = Recipe(
+        name: "Breakfast potato hash",
+        servings: 1,
+        timeCookingInMinutes: 15,
+        timePreparingInMinutes: 5,
+        timeWaitingInMinutes: 0,
+        ingredients: [
+        Ingredient(food: Food(name: "Potatoes"), quantity: 250, unitOfMeasure: "gram", nutritionData: Nutrition(calories: 192, carb: 44, fat: 0, protein: 5)),
+        Ingredient(food: Food(name: "Onions"), quantity: 70, unitOfMeasure: "gram", nutritionData: Nutrition(calories: 28, carb: 7, fat: 0, protein: 1)),
+        Ingredient(food: Food(name: "Chicken ham"), quantity: 100, unitOfMeasure: "gram", nutritionData: Nutrition(calories: 155, carb: 26, fat: 2, protein: 8)),
+        Ingredient(food: Food(name: "Olive oil"), quantity: 7, unitOfMeasure: "gram", nutritionData: Nutrition(calories: 62, carb: 0, fat: 7, protein: 0)),
+        Ingredient(food: Food(name: "Eggs"), quantity: 3, unitOfMeasure: "whole", nutritionData: Nutrition(calories: 210, carb: 3, fat: 15, protein: 18)),
+        Ingredient(food: Food(name: "Soy Sauce"), quantity: 25, unitOfMeasure: "ml", nutritionData: Nutrition(calories: 1, carb: 0, fat: 0, protein: 0)),
+        Ingredient(food: Food(name: "Salt"), quantity: 1, unitOfMeasure: "teaspoon", nutritionData: Nutrition.zero),
+        Ingredient(food: Food(name: "Black pepper"), quantity: 1, unitOfMeasure: "teaspoon", nutritionData: Nutrition.zero),
+        Ingredient(food: Food(name: "Chili powder"), quantity: 1, unitOfMeasure: "teaspoon", nutritionData: Nutrition.zero)],
+        instructions: [
+            Instruction(step: 1, text: "Peel or clean the potatoes with a brush, then cut the potatoes into small cubes, about half inch. Place the potatoes in a bowl and microwave for 6 minutes at 600W"),
+            Instruction(step: 2, text: "Dice onion and chicken ham"),
+            Instruction(step: 3, text: "Heat pan on high heat and add olive oils with microwaved potatoes"),
+            Instruction(step: 4, text: "After potatoes are browned add ham and onion, keep frying for about 3 minutes. Add more oil if needed"),
+            Instruction(step: 5, text: "Make small wells in the pan for the eggs, then crack in the egss, add three splashes of soy sauce and imidiately cover with a lid"),
+            Instruction(step: 6, text: "Cook for 2 to 3 minutes and serve")
+        ],
+        imageData: nil,
+        tags: [Tag(text: "Breakfast"), Tag(text: "Eggs"), Tag(text: "High protein")])
+    
     static let recipieNoPhoto = Recipe(id: UUID(),
                                        name: "KFC chicken",
                                        servings: 2,
