83b934384069d102a09a4c4251e0bea628af3e05
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-24 20:05:02 +0200

    Add MealDataManagerProtocol Add extension to dataManager to conform to MealDataManagerProtocol

--- GymMealPrep/DataManager/DataManager+MealMO.swift
+++ GymMealPrep/DataManager/DataManager+MealMO.swift
@@ -7,3 +7,50 @@
 
 import Foundation
 
+protocol MealDataManagerProtocol {
+    
+    func updateAndSave(meal: Meal)
+    
+    func delete(meal: Meal)
+}
+
+extension DataManager: MealDataManagerProtocol {
+    
+    func updateAndSave(meal: Meal) {
+        let predicate = NSPredicate(format: "id = %@", meal.id as CVarArg)
+        let result = fetchFirst(MealMO.self, predicate: predicate)
+        switch result {
+        case .success(let success):
+            if let mealMO = success {
+                update(mealMO: mealMO, from: meal)
+            } else {
+                mealMO(from: meal)
+            }
+        case .failure(let failure):
+            print("Could not fetch mealMO to update: \(failure.localizedDescription)")
+        }
+        saveContext()
+    }
+    
+    func delete(meal: Meal) {
+        let predicate = NSPredicate(format: "id = %@", meal.id as CVarArg)
+        let result = fetchFirst(MealMO.self, predicate: predicate)
+        switch result {
+        case .success(let success):
+            if let mealMO = success {
+                managedContext.delete(mealMO)
+            }
+        case .failure(let failure):
+            print("Could not fetch mealMO to delete: \(failure.localizedDescription)")
+        }
+        saveContext()
+    }
+    
+    private func mealMO(from meal: Meal) {
+        //TODO: IMPLEMENT MEAL MO CREATION
+    }
+    
+    private func update(mealMO target: MealMO, from source: Meal) {
+        // TODO: IMPLEMENT UPDATE MEAL FUNCTION
+    }
+}
