fc8a2da7150ed6292a8f33fb65b24982fb7e16ea
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-25 09:31:55 +0200

    Add custom init, Disable standard and convenience inits

--- GymMealPrep/CoreDataModel/MealMO+CoreDataClass.swift
+++ GymMealPrep/CoreDataModel/MealMO+CoreDataClass.swift
@@ -12,4 +12,24 @@ import CoreData
 @objc(MealMO)
 public class MealMO: NSManagedObject {
 
+    public init(context: NSManagedObjectContext, id: UUID = UUID(), mealPlanMO: MealPlanMO) {
+        let entityDescription = NSEntityDescription.entity(forEntityName: "MealMO", in: context)!
+        super.init(entity: entityDescription, insertInto: context)
+        self.id = id
+        self.plan = mealPlanMO
+    }
+    
+    @objc override private init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertInto: context)
+    }
+    
+    @available(*, unavailable)
+    public init() {
+        fatalError("\(#function) not implemented")
+    }
+    
+    @available(*, unavailable)
+    public convenience init(context: NSManagedObjectContext) {
+        fatalError("\(#function) not implemented")
+    }
 }
