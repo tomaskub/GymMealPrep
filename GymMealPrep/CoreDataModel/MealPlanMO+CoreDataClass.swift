2c621e720e0f68dcdb20fda2158bc8dc2aef2f5e
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-26 19:15:51 +0200

    Add public init with properties and lock init and conv init

--- GymMealPrep/CoreDataModel/MealPlanMO+CoreDataClass.swift
+++ GymMealPrep/CoreDataModel/MealPlanMO+CoreDataClass.swift
@@ -11,5 +11,23 @@ import CoreData
 
 @objc(MealPlanMO)
 public class MealPlanMO: NSManagedObject {
-
+    public init(context: NSManagedObjectContext, id: UUID = UUID()) {
+        let entityDescription = NSEntityDescription.entity(forEntityName: "MealPlanMO", in: context)!
+        super.init(entity: entityDescription, insertInto: context)
+        self.id = id
+    }
+    
+    @objc override private init(entity: NSEntityDescription, insertInto context: NSManagedObjectContext?) {
+        super.init(entity: entity, insertInto: context)
+    }
+    
+    @available(*, unavailable)
+    public init() {
+        fatalError("\(#function) not implemented")
+    }
+    
+    @available(*, unavailable)
+    public convenience init(context: NSManagedObjectContext) {
+        fatalError("\(#function) not implemented")
+    }
 }
