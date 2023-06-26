4aeb3ce9890f21be8f8cfbb95d1938a143a492d1
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-26 19:01:06 +0200

    Implement update and save func

--- GymMealPrep/DataManager/DataManager+MealPlanMO.swift
+++ GymMealPrep/DataManager/DataManager+MealPlanMO.swift
@@ -16,10 +16,29 @@ protocol MealPlanDataManagerProtocol {
 extension DataManager: MealPlanDataManagerProtocol {
     
     func updateAndSave(mealPlan: MealPlan) {
-        
+        let result = fetchFirst(MealPlanMO.self, predicate: NSPredicate(format: "id = @%", mealPlan.id))
+        switch result {
+        case .success(let success):
+            if let mealPlanMO = success {
+                update(mealPlanMO: mealPlanMO, from: mealPlan)
+            } else {
+                _ = mealPlanMO(from: mealPlan)
+            }
+        case .failure(let failure):
+            print("Could not fetch mealPlanMO to update: \(failure.localizedDescription)")
+        }
+        saveContext()
     }
     
     func delete(mealPlan: MealPlan) {
+        
+    }
+    
+    private func update(mealPlanMO target: MealPlanMO, from source: MealPlan) {
+            // assing values from source to target
+    }
+    
+    private func mealPlanMO(from source: MealPlan) -> MealPlanMO {
         
     }
 }
