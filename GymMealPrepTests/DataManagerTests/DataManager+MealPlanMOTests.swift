bd5c181cb1b0e259b6f81a3b23a5f606cbd183cf
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-28 22:41:04 +0200

    Add test for updating and saving when no meal plan exists

--- GymMealPrepTests/DataManagerTests/DataManager+MealPlanMOTests.swift
+++ GymMealPrepTests/DataManagerTests/DataManager+MealPlanMOTests.swift
@@ -6,8 +6,10 @@
 //
 
 import XCTest
+@testable import GymMealPrep
 
 final class DataManager_MealPlanMOTests: XCTestCase {
+    
     var sut: DataManager!
     
     override func setUp() {
@@ -18,5 +20,24 @@ final class DataManager_MealPlanMOTests: XCTestCase {
         sut = nil
     }
     
-    
+    func testUpdateAndSave_whenNoMealPlanExists() {
+        let mealPlan = MealPlan(name: "Test meal plan", meals: [])
+        
+        sut.updateAndSave(mealPlan: mealPlan)
+        
+        let result = sut.fetchFirst(MealPlanMO.self, predicate: NSPredicate(format: "id = %@", mealPlan.id as CVarArg))
+        
+        switch result {
+        case .success (let success):
+            if let mealPlanMO = success {
+                
+                XCTAssertEqual(mealPlanMO.id, mealPlan.id, "Id of the retrieved mealPlanMO should be equal to id of saved mealPlan")
+                
+            } else {
+                XCTFail("Result of fetch first was not castable as mealPlanMO")
+            }
+        case .failure (let failure):
+            XCTFail("Failed in retrieving saved result: \(failure.localizedDescription)")
+        }
+    }
 }
