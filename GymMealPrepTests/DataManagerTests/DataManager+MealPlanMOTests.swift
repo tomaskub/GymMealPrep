92238f47a7b27e5804542849f2c62e361d18ee56
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-29 01:07:24 +0200

    Add test for updateAndSave when meal plan exists and meal is removed

--- GymMealPrepTests/DataManagerTests/DataManager+MealPlanMOTests.swift
+++ GymMealPrepTests/DataManagerTests/DataManager+MealPlanMOTests.swift
@@ -40,4 +40,30 @@ final class DataManager_MealPlanMOTests: XCTestCase {
             XCTFail("Failed in retrieving saved result: \(failure.localizedDescription)")
         }
     }
+    
+    func testUpdateAndSave_whenMealPlanExists_whenMealRemoved() {
+        let meal = Meal(ingredients: [], recipies: [])
+        var mealPlan = MealPlan(name: "Test meal plan", meals: [meal])
+        sut.updateAndSave(mealPlan: mealPlan)
+        
+        mealPlan.meals = []
+        sut.updateAndSave(mealPlan: mealPlan)
+        let result = sut.fetchFirst(MealPlanMO.self, predicate: NSPredicate(format: "id = %@", mealPlan.id as CVarArg))
+        
+        switch result {
+        case .success(let success):
+            if let mealPlanMO = success {
+                XCTAssertEqual(mealPlanMO.id, mealPlan.id, "Retrieved meal plan id should be equal to saved meal plan id ")
+                if let _meals = mealPlanMO.meals {
+                    XCTAssertEqual(_meals.count, 0, "Count of meals retrieved should be 0")
+                } else {
+                    XCTFail()
+                }
+            } else {
+                XCTFail()
+            }
+        case .failure(let failure):
+            XCTFail()
+        }
+    }
 }
