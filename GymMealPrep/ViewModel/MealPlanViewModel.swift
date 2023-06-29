2495916c8224338d52292425bd073b5e56c9569e
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-29 02:07:22 +0200

    Add MealPlanViewModelProtocol stub Add MealPlanViewModel stub

--- GymMealPrep/ViewModel/MealPlanViewModel.swift
+++ GymMealPrep/ViewModel/MealPlanViewModel.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+protocol MealPlanViewModelProtocol: ObservableObject {
+    var mealPlan: MealPlan { get }
+}
+
+class MealPlanViewModel: MealPlanViewModelProtocol {
+    
+    @Published var mealPlan: MealPlan
+    private var dataManager: MealPlanDataManagerProtocol
+    
+    init(mealPlan: MealPlan, dataManager: MealPlanDataManagerProtocol = DataManager.shared as MealPlanDataManagerProtocol) {
+        self.mealPlan = mealPlan
+        self.dataManager = dataManager
+    }
+}
