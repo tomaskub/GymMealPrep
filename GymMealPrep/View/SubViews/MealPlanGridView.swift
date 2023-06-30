b0e6894c5fb1f6e60517fb02f77de1eb0737d661
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-06-30 21:06:18 +0200

    Add MealPlanGridView

---
+++ GymMealPrep/View/SubViews/MealPlanGridView.swift
@@ -0,0 +1,20 @@
+//
+//  MealPlanGridView.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 6/30/23.
+//
+
+import SwiftUI
+
+struct MealPlanGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MealPlanGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        MealPlanGridView()
+    }
+}
