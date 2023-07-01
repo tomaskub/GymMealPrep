a2e2f20549eed7bea0bb1cdcf583da843e83414c
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-07-01 19:10:45 +0200

    Initial add of MealPlanHostView

---
+++ GymMealPrep/MealPlan/Views/MealPlanHostView.swift
@@ -0,0 +1,20 @@
+//
+//  MealPlanHostView.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 7/1/23.
+//
+
+import SwiftUI
+
+struct MealPlanHostView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MealPlanHostView_Previews: PreviewProvider {
+    static var previews: some View {
+        MealPlanHostView()
+    }
+}
