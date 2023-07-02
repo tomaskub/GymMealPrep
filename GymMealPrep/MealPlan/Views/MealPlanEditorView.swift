a319838091d28eb08ccd7e14d41ad4e54c1bd5c3
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-07-02 13:22:46 +0200

    Add swipe actions for removing a meal part Add a sheet for adding to a meal Add generateRecipeLabel func Remove abstract implementation until concrete implementation is more defined

--- GymMealPrep/MealPlan/Views/MealPlanEditorView.swift
+++ GymMealPrep/MealPlan/Views/MealPlanEditorView.swift
@@ -7,42 +7,73 @@
 
 import SwiftUI
 
-struct MealPlanEditorView: View {
-    @State var draftMealPlan: MealPlan
+struct MealPlanEditorView: View { //<T: MealPlanViewModelProtocol>: View {
+    @ObservedObject var viewModel: MealPlanViewModel
+    @State var selectedMeal: Meal?
     
     var body: some View {
         List {
             
-            Section("Meal plan") {
+            Section("Summary") {
                 
-                Text(draftMealPlan.name ?? "Meal plan")
+                TextField("Meal plan name", text: $viewModel.mealPlanName)
+                    .textFieldStyle(.roundedBorder)
                 
-                RecipeSummaryView(cal: draftMealPlan.nutrition.calories, proteinInGrams: draftMealPlan.nutrition.protein, fatInGrams: draftMealPlan.nutrition.fat, carbInGrams: draftMealPlan.nutrition.carb, format: "%.0f", showLabel: false)
+                RecipeSummaryView(
+                    cal: viewModel.mealPlan.nutrition.calories,
+                    proteinInGrams: viewModel.mealPlan.nutrition.protein,
+                    fatInGrams: viewModel.mealPlan.nutrition.fat,
+                    carbInGrams: viewModel.mealPlan.nutrition.carb,
+                    format: "%.0f",
+                    showLabel: false)
                     .frame(maxWidth: .infinity)
                     .font(.caption)
             } // END OF SECTION
             .listRowSeparator(.hidden)
             
-            ForEach(Array(draftMealPlan.meals.enumerated()), id: \.element) { index, meal  in
+            ForEach(Array(viewModel.mealPlan.meals.enumerated()), id: \.element) { index, meal  in
                 
                 Section {
-                    RecipeSummaryView(cal: meal.nutrition.calories, proteinInGrams: meal.nutrition.protein, fatInGrams: meal.nutrition.fat, carbInGrams: meal.nutrition.carb, format: "%.0f", showLabel: false)
+                    
+                    RecipeSummaryView(
+                        cal: meal.nutrition.calories,
+                        proteinInGrams: meal.nutrition.protein,
+                        fatInGrams: meal.nutrition.fat,
+                        carbInGrams: meal.nutrition.carb,
+                        format: "%.0f",
+                        showLabel: false)
                         .frame(maxWidth: .infinity)
                         .font(.caption)
                     
                     ForEach(meal.recipes) { recipe in
-                        Text(recipe.name)
+                        Text(generateRecipeLabel(recipe: recipe))
+                            .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                                Button(role: .destructive) {
+                                    viewModel.removeFromMeal(meal: meal, recipe)
+                                } label: {
+                                    Label("Delete", systemImage: "trash")
+                                }
+                            }
                     } // END OF FOR EACH
-                    
+
                     ForEach(meal.ingredients) { ingredient in
                         Text(generateIngredientLabel(ingredient:ingredient))
+                            .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                                Button(role: .destructive) {
+                                    viewModel.removeFromMeal(meal: meal, ingredient)
+                                } label: {
+                                    Label("Delete", systemImage: "trash")
+                                }
+                            }
                     } // END OF FOR EACH
-                    
                     HStack {
                         Spacer()
-                        Text("Add to meal ")
+                        Text("Add to meal")
                         Spacer()
                     } // END OF H STACK
+                    .onTapGesture {
+                        selectedMeal = meal
+                    }
                     
                 } header: {
                         Text("Meal #\(index + 1)")
@@ -50,15 +81,26 @@ struct MealPlanEditorView: View {
                 .headerProminence(.increased)
             } // END OF FOR EACH
         } // END OF LIST
+        .sheet(item: $selectedMeal) { _ in
+            Text("Placeholder for editor sheet view")
+            //TODO: REPLACE WITH IMPLEMENTATION OF MEALPLANEDITORSHEETVIEW WHEN READY
+//            MealPlanEditorSheetView()
+        }
     } // END OF BODY
     
     func generateIngredientLabel(ingredient: Ingredient) -> String {
         return ingredient.food.name + " " + String(ingredient.quantity) + " " + ingredient.unitOfMeasure
     }
+    
+    func generateRecipeLabel(recipe: Recipe) -> String {
+        return recipe.name
+    }
+    
 } // END OF STRUCT
 
 struct MealPlanEditorView_Previews: PreviewProvider {
     static var previews: some View {
-        MealPlanEditorView(draftMealPlan: SampleData.sampleMealPlan)
+        MealPlanEditorView(viewModel: MealPlanViewModel(mealPlan: SampleData.sampleMealPlan, dataManager: DataManager.preview))
+        
     }
 }
