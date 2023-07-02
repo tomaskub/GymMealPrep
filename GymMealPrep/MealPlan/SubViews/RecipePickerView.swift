a2d460c74837b088f3c2a8f66595f588c9cc1c97
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-07-02 13:20:25 +0200

    Initial add

---
+++ GymMealPrep/MealPlan/SubViews/RecipePickerView.swift
@@ -0,0 +1,20 @@
+//
+//  RecipePickerView.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 7/2/23.
+//
+
+import SwiftUI
+
+struct RecipePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RecipePickerView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipePickerView()
+    }
+}
