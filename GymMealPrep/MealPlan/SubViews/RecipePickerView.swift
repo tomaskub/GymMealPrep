852556a63e31dfe852fb0157b9b3f9c4422cab2a
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-07-02 14:16:19 +0200

    Replace properties in the struct with a simple view model class

--- GymMealPrep/MealPlan/SubViews/RecipePickerView.swift
+++ GymMealPrep/MealPlan/SubViews/RecipePickerView.swift
@@ -6,34 +6,56 @@
 //
 
 import SwiftUI
+import Combine
 
 protocol RecipeSaveHandler {
     func addRecipe(_: Recipe)
 }
 
+class RecipePickerViewModel: ObservableObject {
+    @Published var searchTerm: String
+    @Published var retrievedRecipes: [Recipe]
+    
+    private var dataManager: DataManager
+    
+    init(searchTerm: String = String(), retrievedRecipes: [Recipe] = [Recipe](), dataManager: DataManager = .shared) {
+        self.searchTerm = searchTerm
+        self.retrievedRecipes = retrievedRecipes
+        self.dataManager = dataManager
+    }
+    
+    func searchRecipes() {
+        if searchTerm.isEmpty {
+            retrievedRecipes = dataManager.recipeArray
+        } else {
+            retrievedRecipes = dataManager.recipeArray.filter({ $0.name.lowercased().contains(searchTerm.lowercased())
+            })
+        }
+    }
+}
 struct RecipePickerView: View {
     
     @Environment(\.dismiss) var dismiss
-    
     var saveHandler: RecipeSaveHandler
-    @State var searchTerm: String = String()
-    @State var recipes = [Recipe]()
+    @StateObject var viewModel: RecipePickerViewModel
     
     var body: some View {
         VStack {
+            
             HStack {
-            TextField("Type in recipe name", text: $searchTerm)
+                TextField("Type in recipe name", text: $viewModel.searchTerm)
                 .textFieldStyle(.roundedBorder)
                 Button {
-                    //search for recipe and populate the list
-                    recipes.append(SampleData.recipeBreakfastPotatoHash)
+                    viewModel.searchRecipes()
                 } label: {
                     Image(systemName: "magnifyingglass.circle.fill")
                         .font(.title)
                 }
-            }.padding(.horizontal)
+            }
+            .padding(.horizontal)
+            
             List {
-                ForEach(recipes) { recipe in
+                ForEach(viewModel.retrievedRecipes) { recipe in
                     RecipeListRowView(recipe)
                         .onTapGesture {
                             saveHandler.addRecipe(recipe)
@@ -53,6 +75,6 @@ struct RecipePickerView_Previews: PreviewProvider {
         }
     }
     static var previews: some View {
-        RecipePickerView(saveHandler: PreviewSaveHandler())
+        RecipePickerView(saveHandler: PreviewSaveHandler(), viewModel: RecipePickerViewModel(dataManager: .preview))
     }
 }
