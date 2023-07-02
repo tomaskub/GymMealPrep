d5a18083502e6893469f918e2d8b549cd5114ddf
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-07-02 13:18:23 +0200

    Add additional variables and func in the protocol, Add implementation for variable Add placeholder implementation for functions

--- GymMealPrep/MealPlan/ViewModels/MealPlanViewModel.swift
+++ GymMealPrep/MealPlan/ViewModels/MealPlanViewModel.swift
@@ -9,15 +9,35 @@ import Foundation
 
 protocol MealPlanViewModelProtocol: ObservableObject {
     var mealPlan: MealPlan { get }
+    var mealPlanName: String { get set }
+    
+    func removeFromMeal(meal: Meal, _: Ingredient)
+    
+    func removeFromMeal(meal: Meal, _: Recipe)
+    
 }
 
 class MealPlanViewModel: MealPlanViewModelProtocol {
     
+    @Published var mealPlanName: String
     @Published var mealPlan: MealPlan
     private var dataManager: MealPlanDataManagerProtocol
     
     init(mealPlan: MealPlan, dataManager: MealPlanDataManagerProtocol = DataManager.shared as MealPlanDataManagerProtocol) {
         self.mealPlan = mealPlan
         self.dataManager = dataManager
+        if let name = mealPlan.name {
+            self.mealPlanName = name
+        } else {
+            self.mealPlanName = String()
+        }
+    }
+    
+    func removeFromMeal(meal: Meal, _: Ingredient) {
+        //TODO: IMPLEMENT REMOVING INGREDIENT FROM MEAL
+    }
+    
+    func removeFromMeal(meal: Meal, _: Recipe) {
+        //TODO: IMPLEMENT REMOVING RECIPE FROM MEAL
     }
 }
