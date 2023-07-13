8ec9d6c035f33925684cbd51842d8e8614c9498b
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-07-13 22:29:19 +0200

    Add definition of keyboard readable protocol and a default implementation

--- GymMealPrep/Helpers/KeyboardReadable.swift
+++ GymMealPrep/Helpers/KeyboardReadable.swift
@@ -5,4 +5,26 @@
 //  Created by Tomasz Kubiak on 7/13/23.
 //
 
-import Foundation
+import Combine
+import UIKit
+
+protocol KeyboardReadable {
+    var keyboardPublisher: AnyPublisher<Bool, Never> { get }
+}
+
+extension KeyboardReadable {
+    var keyboardPublisher: AnyPublisher<Bool, Never> {
+        Publishers.Merge(
+            NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)
+                .map({ _ in
+                    true
+                }),
+            NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
+                .map({ _ in
+                    false
+                })
+        )
+        .eraseToAnyPublisher()
+    }
+                                            
+}
