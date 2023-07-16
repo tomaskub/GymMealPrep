9a87e1cb1b80185ba69642d623dc7d7553c5a7f7
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-07-16 15:47:21 +0200

    Add test confirming all of the ui elements exists on navigating to the view

--- GymMealPrepUITests/RecipeListUITests.swift
+++ GymMealPrepUITests/RecipeListUITests.swift
@@ -42,6 +42,31 @@ final class RecipeListUITests: XCTestCase {
         let result = navTitleText.waitForExistence(timeout: standardTimeout)
         XCTAssertTrue(result, "Recipe navigation title should exist")
     }
+    
+    func test_RecipeList_UIComponents_arePresent() throws {
+        // Given
+        navigateToRecipeList()
+        
+        let recipesNavigationBar = app.navigationBars["Recipes"]
+        let navTitleText = recipesNavigationBar.staticTexts["Recipes"]
+        let addButton = recipesNavigationBar.buttons["Add"]
+        let chevronLeftButton = recipesNavigationBar.images["Back"]
+        let addFromTextButton = recipesNavigationBar.buttons["Add from text"]
+        
+        // When
+        chevronLeftButton.tap()
+        
+        //Then
+        let existsPredicate = NSPredicate(format: "exists == true")
+        let expectations = [
+        expectation(for: existsPredicate, evaluatedWith: navTitleText),
+        expectation(for: existsPredicate, evaluatedWith: addButton),
+        expectation(for: existsPredicate, evaluatedWith: chevronLeftButton),
+        expectation(for: existsPredicate, evaluatedWith: addFromTextButton)
+        ]
+        let testResult = XCTWaiter.wait(for: expectations, timeout: standardTimeout)
+        XCTAssertEqual(testResult, .completed, "All of the elements should exists")
+    }
 }
 
 extension RecipeListUITests {
