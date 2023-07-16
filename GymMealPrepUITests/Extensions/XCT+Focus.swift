2831761750ea5b1750c586e2a61ddbb40cefc5b7
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-07-16 12:05:11 +0200

    Add helper func to wait until element has keyboard focus

--- GymMealPrepUITests/Extensions/XCT+Focus.swift
+++ GymMealPrepUITests/Extensions/XCT+Focus.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+import XCTest
+
+extension XCTestCase {
+    func waitUtilElementHasKeyboardFocus(element: XCUIElement, timeout: TimeInterval, file: StaticString = #file, line: UInt = #line) -> XCUIElement {
+        let expectation = expectation(description: "waiting for element \(element) to have focus")
+        
+        let timer = Timer(timeInterval: 1, repeats: true) { timer in
+            guard element.value(forKey: "hasKeyboardFocus") as? Bool ?? false else { return }
+            
+            expectation.fulfill()
+            timer.invalidate()
+        }
+        
+        RunLoop.current.add(timer, forMode: .common)
+        wait(for: [expectation], timeout: timeout)
+        return element
+    }
+}
