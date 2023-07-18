df1788307de134542e4072ffb47cf2204d29d86b
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-07-18 15:36:34 +0200

    Add MealPlanTab UI tests xcTestCase

---
+++ GymMealPrepUITests/MealPlanTabUITests.swift
@@ -0,0 +1,34 @@
+//
+//  MealPlanTabUITests.swift
+//  GymMealPrepUITests
+//
+//  Created by Tomasz Kubiak on 7/18/23.
+//
+
+import XCTest
+
+final class MealPlanTabUITests: XCTestCase {
+
+    
+    //MARK: INFORMATION ABOUT NAMING
+    /*
+     Naming structure: test_UnitOfWork_StateUnderTest_ExpectedBehaviour
+     Naming structure: test_[Struct or class]_[UI component]_[expected result]
+     Testing structure: Given, When, Then
+     */
+
+    var app: XCUIApplication!
+    
+    let standardTimeout = 2.5
+    
+    override func setUp() {
+        app = XCUIApplication()
+        continueAfterFailure = false
+        app.launchArguments = ["-UITests"]
+        app.launch()
+    }
+
+    override func tearDown() {
+        app = nil
+    }
+}
