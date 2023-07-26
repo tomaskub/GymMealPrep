25c428a481a4995124c93213f98ef4dca62e78af
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-07-26 21:36:35 +0200

    Add func clearAndTypeText to XCUI Elements

--- GymMealPrepUITests/Extensions/XCUIElement+Extensions.swift
+++ GymMealPrepUITests/Extensions/XCUIElement+Extensions.swift
@@ -9,10 +9,23 @@ import Foundation
 import XCTest
 
 extension XCUIElement {
+    
     func waitForNonExistence(timeout: TimeInterval) -> Bool {
         let predicate = NSPredicate(format: "exists == false")
         let expectation = XCTNSPredicateExpectation(predicate: predicate, object: self)
         _ = XCTWaiter.wait(for: [expectation], timeout: timeout)
         return !exists
+    }
+    
+    func clearAndTypeText(_ text: String) {
+        guard let stringValue = self.value as? String else { return }
+        
+        var deleteString = String()
+        for _ in stringValue {
+            deleteString += XCUIKeyboardKey.delete.rawValue
+        }
+        
+        self.typeText(deleteString)
+        self.typeText(text)
     }
 }
