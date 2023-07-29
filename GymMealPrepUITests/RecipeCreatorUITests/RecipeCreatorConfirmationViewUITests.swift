53305f49ab58ef20d4d7fab03f69ed915bb92319
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-07-29 20:03:37 +0200

    Add test confirming all ui elements exists on confirmation screen

--- GymMealPrepUITests/RecipeCreatorUITests/RecipeCreatorConfirmationViewUITests.swift
+++ GymMealPrepUITests/RecipeCreatorUITests/RecipeCreatorConfirmationViewUITests.swift
@@ -29,6 +29,37 @@ final class RecipeCreatorConfirmationViewUITests: XCTestCase {
     }
     
     // Confirm all of the elements exist on showing the screen
+    func test_RecipeCreatorConfirmationView_UIElements_exist() {
+        // Given
+        navigateToRecipeCreatorConfirmationView()
+        
+        let navigationTitle = app.navigationBars.staticTexts["Add details"]
+        let photoSectionHeader = app.collectionViews.staticTexts["PHOTO"]
+        let timeCookingSectionHeader = app.collectionViews.staticTexts["TIME COOKING"]
+        let tagsSectionHeader = app.collectionViews.staticTexts["TAGS"]
+        
+        let cookingTimeStaticText = app.collectionViews.staticTexts["Cooking time:"]
+        let preparingTimeStaticText = app.collectionViews.staticTexts["Preparing time:"]
+        let waitingTimeStaticText = app.collectionViews.staticTexts["Time waiting:"]
+        
+        let cookingTimeTextField = app.collectionViews.cells.containing(.staticText, identifier: "Cooking time:").textFields["minutes"]
+        let preparingTimeTextField = app.collectionViews.cells.containing(.staticText, identifier: "Preparing time:").textFields["minutes"]
+        let waitingTimeTextField = app.collectionViews.cells.containing(.staticText, identifier: "Time waiting:").textFields["minutes"]
+        
+        let addPhotoButton = app.collectionViews.buttons["add-change-photo"]
+        let addTagButton = app.collectionViews.buttons["Add"]
+        let tagTextField = app.collectionViews.cells.containing(.button, identifier: "Add").textFields["Add new tag"]
+        
+        // Then
+        let predicate = NSPredicate(format: "exists == true")
+        var expectations = [XCTestExpectation]()
+        for element in [navigationTitle, photoSectionHeader, timeCookingSectionHeader, tagsSectionHeader, cookingTimeStaticText, preparingTimeStaticText, waitingTimeStaticText, cookingTimeTextField, preparingTimeTextField, waitingTimeTextField, addPhotoButton, addTagButton, tagTextField] {
+            expectations.append(expectation(for: predicate, evaluatedWith: element))
+        }
+        let result = XCTWaiter.wait(for: expectations, timeout: standardTimeout)
+        XCTAssertEqual(result, .completed, "All elements should exist")
+    }
+    
     // Confirm adding tags works
     // Confirm removing tags work
     // Confirm photo part works
