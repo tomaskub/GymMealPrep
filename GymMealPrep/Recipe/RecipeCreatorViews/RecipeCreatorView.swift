9ff341ff450f2324e64795bc3882b1a51b0b560d
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-08-04 18:39:10 +0200

    Change accessibility identifiers for: - recipe title text field - servings quantity stepper - ingredients tool tip - instructions tool tip

--- GymMealPrep/Recipe/RecipeCreatorViews/RecipeCreatorView.swift
+++ GymMealPrep/Recipe/RecipeCreatorViews/RecipeCreatorView.swift
@@ -24,7 +24,7 @@ struct RecipeCreatorView: View {
             VStack(alignment: .leading) {
                 
                 TextField("Recipe title", text: $viewModel.recipeTitle)
-                    .accessibilityIdentifier("RecipeTitleTextField")
+                    .accessibilityIdentifier("recipe-title-text-field")
                     .focused($textFieldInFocus, equals: .title)
                     .font(.title3)
                     .fontWeight(.semibold)
@@ -49,7 +49,7 @@ struct RecipeCreatorView: View {
                     Spacer()
                     Stepper("\(viewModel.servings) \(stepperLabel)", value: $viewModel.servings)
                         .fixedSize()
-                        .accessibilityIdentifier("ServingsQuantityStepper")
+                        .accessibilityIdentifier("servings-quantity-stepper")
                 }
                 
                 TextEditor(text: $viewModel.ingredientsEntry)
@@ -75,7 +75,7 @@ struct RecipeCreatorView: View {
                                 
                                 Text("2 slices of bacon\n1 egg")
                             } // END OF VSTACK
-                            .accessibilityIdentifier("IngredientsToolTip")
+                            .accessibilityIdentifier("ingredients-tool-tip")
                             .padding()
                             .background(
                                 RoundedRectangle(cornerRadius: 20)
@@ -135,7 +135,7 @@ struct RecipeCreatorView: View {
                                 
                                 Text("1. First sample instruction.\n2. Second dample instruction")
                             } // END OF VSTACK
-                            .accessibilityIdentifier("InstructionsToolTip")
+                            .accessibilityIdentifier("instructions-tool-tip")
                             .padding()
                             .background(
                                 RoundedRectangle(cornerRadius: 20)
