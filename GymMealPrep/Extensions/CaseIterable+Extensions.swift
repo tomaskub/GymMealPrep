d95af61ddcd8c804426272b0cb6407c14c7bae35
Tomasz Kubiak <tomasz.kubiak92@gmail.com>
2023-08-06 21:59:59 +0200

    Add generic next and previous func to case iterable

--- GymMealPrep/Extensions/CaseIterable+Extensions.swift
+++ GymMealPrep/Extensions/CaseIterable+Extensions.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+
+extension CaseIterable where Self: Equatable {
+    
+    ///Returns the next enum case or the first one in case it was called on last one
+    func next() -> Self {
+        let all = Self.allCases
+        let index = all.firstIndex(of: self)!
+        let next = all.index(after: index)
+        return all[next == all.endIndex ? all.startIndex : next]
+    }
+    
+    ///Returns the previous enum case or the last one in case it was called on the first one
+    func previous() -> Self {
+        let all = Self.allCases.reversed()
+        let index = all.firstIndex(of: self)!
+        let next = all.index(after: index)
+        return all[next == all.endIndex ? all.startIndex : next]
+    }
+}
