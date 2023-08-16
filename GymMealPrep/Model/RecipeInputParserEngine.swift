31dfe5c7422ef3641afd51b5ba375709ab087110
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-08-16 20:46:47 +0200

    Add basic func to detect bullet points in input to RecipeInputParserEngine

--- GymMealPrep/Model/RecipeInputParserEngine.swift
+++ GymMealPrep/Model/RecipeInputParserEngine.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+class RecipeInputParserEngine {
+    private let input: String
+    
+    init(input: String) {
+        self.input = input
+    }
+    
+    /// Return character used for list starting symbol if any
+    func findListSymbol() -> Character? {
+        guard !input.isEmpty else { return nil }
+        let scanner = Scanner(string: input)
+        scanner.charactersToBeSkipped = nil
+        var result: [Character : Int] = [:]
+        var lastChar = input.first!
+        while !scanner.isAtEnd {
+            if let currentChar = scanner.scanCharacter(){
+                if !currentChar.isNewline && lastChar.isNewline {
+                    let oldValue = result[currentChar] ?? 0
+                    result.updateValue(oldValue + 1, forKey: currentChar)
+                }
+                lastChar = currentChar
+            }
+        }
+        // find the symbol with max repetitions
+        if let symbol = result.max(by: { a, b in a.value < b.value }) {
+            return symbol.key
+        } else {
+            return nil
+        }
+    }
+}
