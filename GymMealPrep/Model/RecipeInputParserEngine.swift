e11d931b584d3efd3a67cc40ef589d7efdc35d5d
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-08-16 21:43:31 +0200

    Add errors Add functions to parse new line and to parse ingredients Add unit tests for finding list punctuation and ingredient parsing

--- GymMealPrep/Model/RecipeInputParserEngine.swift
+++ GymMealPrep/Model/RecipeInputParserEngine.swift
@@ -7,6 +7,11 @@
 
 import Foundation
 
+enum RecipeInputParserEngineError: Error {
+    case emptyInput
+    case couldNotDetermineSymbol
+}
+
 class RecipeInputParserEngine {
     private let input: String
     
@@ -15,8 +20,8 @@ class RecipeInputParserEngine {
     }
     
     /// Return character used for list starting symbol if any
-    func findListSymbol() -> Character? {
-        guard !input.isEmpty else { return nil }
+    func findListSymbol() throws -> Character {
+        guard !input.isEmpty else { throw RecipeInputParserEngineError.emptyInput }
         let scanner = Scanner(string: input)
         scanner.charactersToBeSkipped = nil
         var result: [Character : Int] = [:]
@@ -34,7 +39,44 @@ class RecipeInputParserEngine {
         if let symbol = result.max(by: { a, b in a.value < b.value }) {
             return symbol.key
         } else {
-            return nil
+            throw RecipeInputParserEngineError.couldNotDetermineSymbol
         }
     }
+    
+    func parseIngredients() throws -> [String] {
+        guard !input.isEmpty else { throw RecipeInputParserEngineError.emptyInput }
+        var result = [String]()
+        // attempt to find delimiter
+        do {
+            let bullet = try findListSymbol()
+            let charSet = CharacterSet(charactersIn: String("\(bullet)"))
+            let trimmingCharSet = charSet.union(CharacterSet.whitespacesAndNewlines)
+            result = input.components(separatedBy: charSet).map { value in
+                value.trimmingCharacters(in: trimmingCharSet)
+            }
+            result.removeAll(where: { value in
+                return value.isEmpty
+            })
+        } catch RecipeInputParserEngineError.couldNotDetermineSymbol {
+            // parse by new lines
+            let scanner = Scanner(string: input)
+            while !scanner.isAtEnd {
+                if let newLine = scanNewLine(scanner: scanner) {
+                    result.append(newLine)
+                }
+            }
+        }
+        return result
+    }
+    
+    private func scanNewLine(scanner: Scanner) -> String? {
+        var currentChar: Character = Character("\n")
+        while currentChar.isNewline && !scanner.isAtEnd {
+            currentChar = scanner.scanCharacter() ?? Character("\n")
+        }
+        var newLine: String = String(currentChar)
+        newLine.append(scanner.scanUpToCharacters(from: .newlines) ?? String())
+        return newLine
+    }
+    
 }
--- GymMealPrepTests/RecipeInputParserEngineTests.swift
+++ GymMealPrepTests/RecipeInputParserEngineTests.swift
@@ -4,10 +4,12 @@
 //
 //  Created by Tomasz Kubiak on 16/08/2023.
 //
-
+@testable import GymMealPrep
 import XCTest
 
 final class RecipeInputParserEngineTests: XCTestCase {
+    
+    var sut: RecipeInputParserEngine!
     
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -16,14 +18,20 @@ final class RecipeInputParserEngineTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func test_findListSymbol_whenSameSymbolOnNewLine() throws {
+        sut = RecipeInputParserEngine(input: InputStaticStrings.ingredientsWithBulletList)
+        let result = try sut.findListSymbol()
+        XCTAssertEqual(result, "•")
     }
+    
+    func test_parseIngredients() throws {
+        sut = RecipeInputParserEngine(input: InputStaticStrings.ingredientsWithBulletList)
+        let result = try sut.parseIngredients()
+        let expectation = ["1 - Whole Bulb Garlic", "2 ½ Tbsp (50g) - Natural Greek Yogurt","1 - Garlic Clove, Minced","¼ - Lemon, Zested", "¼ - Lime, Zest","¼ - Lemon, Juiced", "3 Tbsp (42g) - Butter", "1 ½ tsp (4g) - Smoked Paprika", "½ (2g) - Dried Chilli Flakes", "3 tsp (15ml) - White Vinegar", "2-4 - Free Range Eggs", "1 ½ tsp (7.5ml) - Extra Virgin Olive Oil (Optional)", "Brioche Bread Or Bread Of Choice, Lightly Toasted", "Seasoning To Taste", "Dill To Garnish", "Parsley To Garnish"]
+        XCTAssertEqual(result.first, expectation.first)
+    }
+    
     
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
@@ -32,4 +40,38 @@ final class RecipeInputParserEngineTests: XCTestCase {
         }
     }
 
+}
+
+extension RecipeInputParserEngineTests {
+   
+    
+  
+    struct InputStaticStrings {
+        static let ingredientsWithBulletList = """
+        •1 - Whole Bulb Garlic
+        •2 ½ Tbsp (50g) - Natural Greek Yogurt
+        •1 - Garlic Clove, Minced
+        •¼ - Lemon, Zested
+        •¼ - Lime, Zest
+        •¼ - Lemon, Juiced
+        •3 Tbsp (42g) - Butter
+        •1 ½ tsp (4g) - Smoked Paprika
+        •½ (2g) - Dried Chilli Flakes
+        •3 tsp (15ml) - White Vinegar
+        •2-4 - Free Range Eggs
+        •1 ½ tsp (7.5ml) - Extra Virgin Olive Oil (Optional)
+        •Brioche Bread Or Bread Of Choice, Lightly Toasted
+        •Seasoning To Taste
+        •Dill To Garnish
+        •Parsley To Garnish
+        """
+        static let instructionsWithBulletList =
+            """
+        •Preheat oven to 180.c - 350.f. Place the whole garlic bulb onto a sheet of aluminium foil, drizzle over the oil, fold into a parcel, and roast in the oven for 45 minutes or until soft and tender. Remove and let cool slightly.
+        •Place a small pan over medium heat; add the butter, smoked paprika, chilli flakes and seasoning to taste. Melt and cook for 2 minutes. Remove and set aside.
+        •In the meantime, prepare the citrus yogurt sauce by placing the yogurt into a bowl, adding the minced garlic, lemon and lime zest, lemon juice and seasoning to taste.
+        •Bring a saucepan of water over high heat, add the vinegar and bring to a boil. Poach the eggs for 2 1/2 minutes or until done to your liking. Remove and drain on a paper towel.
+        •Toast the bread to your liking, and spread with the roasted garlic and citrus yogurt dressing. Add the poached eggs and drizzle with the smoked paprika and chilli butter. Garnish with dill, parsley and cracked black pepper.
+        """
+    }
 }
