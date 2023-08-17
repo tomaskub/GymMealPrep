6322a25080e5f4cb52292e5d21e16038ea192a89
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-08-17 14:57:05 +0200

    Remove existing tests Implement functions generating input for future test

--- GymMealPrepTests/RecipeInputParserEngineTests.swift
+++ GymMealPrepTests/RecipeInputParserEngineTests.swift
@@ -9,29 +9,19 @@ import XCTest
 
 final class RecipeInputParserEngineTests: XCTestCase {
     
+    //MARK: LIST DELIMITER STYLES TESTED BELOW
+    // basic lists - list with a singular delimiter
+    // iterated list - list with a letter or number increasing
+    
     var sut: RecipeInputParserEngine!
     
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        sut = RecipeInputParserEngine(input: "test")
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
     }
-    
-    func test_findListSymbol_whenSameSymbolOnNewLine() throws {
-        sut = RecipeInputParserEngine(input: InputStaticStrings.ingredientsWithBulletList)
-        let result = try sut.findListSymbol()
-        XCTAssertEqual(result, CharacterSet(charactersIn: "•"))
-    }
-    
-    func test_parseIngredients() throws {
-        sut = RecipeInputParserEngine(input: InputStaticStrings.ingredientsWithBulletList)
-        let result = try sut.parseIngredients()
-        let expectation = ["1 - Whole Bulb Garlic", "2 ½ Tbsp (50g) - Natural Greek Yogurt","1 - Garlic Clove, Minced","¼ - Lemon, Zested", "¼ - Lime, Zest","¼ - Lemon, Juiced", "3 Tbsp (42g) - Butter", "1 ½ tsp (4g) - Smoked Paprika", "½ (2g) - Dried Chilli Flakes", "3 tsp (15ml) - White Vinegar", "2-4 - Free Range Eggs", "1 ½ tsp (7.5ml) - Extra Virgin Olive Oil (Optional)", "Brioche Bread Or Bread Of Choice, Lightly Toasted", "Seasoning To Taste", "Dill To Garnish", "Parsley To Garnish"]
-        XCTAssertEqual(result.first, expectation.first)
-    }
-    
     
 //    func testPerformanceExample() {
 //        sut = RecipeInputParserEngine(input: InputStaticStrings.ingredientsWithBulletList)
@@ -43,35 +33,77 @@ final class RecipeInputParserEngineTests: XCTestCase {
 }
 
 extension RecipeInputParserEngineTests {
-   
+    // Test for input generation - prints into console generated input
+    /*
+    func test_inputGenerations() {
+        let input = InputStaticStrings.ingredientsArray
+        
+        let simple = generateInputWithSimpleDelimiter(from: input, delimiter: "-")
+        print("Basic iterator generates:\n\n\(simple)\n\n")
+        let first = generateInputWithIteratedDelimiter(from: input, iteratorType: .alphabetical)
+        print("Alphabetical iterator generates:\n\n\(first)\n\n")
+        let second = generateInputWithIteratedDelimiter(from: input, iteratorType: .numerical)
+        print("Numerical iterator generates:\n\n\(second)\n\n")
+    }
+    */
+    fileprivate enum ListIteratorType {
+        case numerical
+        case alphabetical
+    }
+    fileprivate func generateInputWithSimpleDelimiter(from source: [String], delimiter: Character) -> String {
+        let array = source.map { element in
+            "\(delimiter)"+element+"\n"
+        }
+        return array.reduce(String()) { a, b in
+            a.isEmpty ? b : a+b
+        }
+    }
     
-  
+    fileprivate func generateInputWithIteratedDelimiter(from source: [String], iteratorType: ListIteratorType) -> String {
+        var result = [String]()
+        switch iteratorType {
+        case .numerical:
+            for (index, element) in source.enumerated() {
+                let newElement = "\(index + 1). "+element+"\n"
+                result.append(newElement)
+            }
+        case .alphabetical:
+            
+            for (index, element) in source.enumerated() {
+                let iterator = returnLetterIterator(index: index)
+                let newElement = "\(iterator). "+element+"\n"
+                result.append(newElement)
+            }
+        }
+        
+        return result.reduce(String()) { a, b in
+            a.isEmpty ? b : a+b
+        }
+    }
+    private func returnLetterIterator(index: Int) -> String {
+        let letters = "abcdefghijklmnopqrstuvwxyz"
+        if Double(index + 1) / Double(letters.count) <= Double(1) {
+            let iteratorIndex = letters.index(letters.startIndex, offsetBy: index)
+            let iterator = letters[iteratorIndex]
+            return String(iterator)
+        } else {
+            let firstIteratorIndex = letters.index(letters.startIndex, offsetBy: index/26)
+            let firstIterator = letters[firstIteratorIndex]
+            let secondIteratorIndex = letters.index(letters.startIndex, offsetBy: index%26)
+            let secondIterator = letters[secondIteratorIndex]
+            return String(firstIterator)+String(secondIterator)
+        }
+    }
     struct InputStaticStrings {
-        static let ingredientsWithBulletList = """
-        •1 - Whole Bulb Garlic
-        •2 ½ Tbsp (50g) - Natural Greek Yogurt
-        •1 - Garlic Clove, Minced
-        •¼ - Lemon, Zested
-        •¼ - Lime, Zest
-        •¼ - Lemon, Juiced
-        •3 Tbsp (42g) - Butter
-        •1 ½ tsp (4g) - Smoked Paprika
-        •½ (2g) - Dried Chilli Flakes
-        •3 tsp (15ml) - White Vinegar
-        •2-4 - Free Range Eggs
-        •1 ½ tsp (7.5ml) - Extra Virgin Olive Oil (Optional)
-        •Brioche Bread Or Bread Of Choice, Lightly Toasted
-        •Seasoning To Taste
-        •Dill To Garnish
-        •Parsley To Garnish
-        """
-        static let instructionsWithBulletList =
-            """
-        •Preheat oven to 180.c - 350.f. Place the whole garlic bulb onto a sheet of aluminium foil, drizzle over the oil, fold into a parcel, and roast in the oven for 45 minutes or until soft and tender. Remove and let cool slightly.
-        •Place a small pan over medium heat; add the butter, smoked paprika, chilli flakes and seasoning to taste. Melt and cook for 2 minutes. Remove and set aside.
-        •In the meantime, prepare the citrus yogurt sauce by placing the yogurt into a bowl, adding the minced garlic, lemon and lime zest, lemon juice and seasoning to taste.
-        •Bring a saucepan of water over high heat, add the vinegar and bring to a boil. Poach the eggs for 2 1/2 minutes or until done to your liking. Remove and drain on a paper towel.
-        •Toast the bread to your liking, and spread with the roasted garlic and citrus yogurt dressing. Add the poached eggs and drizzle with the smoked paprika and chilli butter. Garnish with dill, parsley and cracked black pepper.
-        """
+        static let ingredientsArray = ["1 - Whole Bulb Garlic", "2 ½ Tbsp (50g) - Natural Greek Yogurt","1 - Garlic Clove, Minced","¼ - Lemon, Zested", "¼ - Lime, Zest","¼ - Lemon, Juiced", "3 Tbsp (42g) - Butter", "1 ½ tsp (4g) - Smoked Paprika", "½ (2g) - Dried Chilli Flakes", "3 tsp (15ml) - White Vinegar", "2-4 - Free Range Eggs", "1 ½ tsp (7.5ml) - Extra Virgin Olive Oil (Optional)", "Brioche Bread Or Bread Of Choice, Lightly Toasted", "Seasoning To Taste", "Dill To Garnish", "Parsley To Garnish"]
+        
+        static let instructionsArray =
+        [
+            "Preheat oven to 180.c - 350.f. Place the whole garlic bulb onto a sheet of aluminium foil, drizzle over the oil, fold into a parcel, and roast in the oven for 45 minutes or until soft and tender. Remove and let cool slightly",
+            "Place a small pan over medium heat; add the butter, smoked paprika, chilli flakes and seasoning to taste. Melt and cook for 2 minutes. Remove and set aside.",
+            "In the meantime, prepare the citrus yogurt sauce by placing the yogurt into a bowl, adding the minced garlic, lemon and lime zest, lemon juice and seasoning to taste.",
+            "Bring a saucepan of water over high heat, add the vinegar and bring to a boil. Poach the eggs for 2 1/2 minutes or until done to your liking. Remove and drain on a paper towel.",
+            "Toast the bread to your liking, and spread with the roasted garlic and citrus yogurt dressing. Add the poached eggs and drizzle with the smoked paprika and chilli butter. Garnish with dill, parsley and cracked black pepper."
+        ]
     }
 }
