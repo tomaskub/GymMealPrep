509319620d3185d3246ce311f19010654908aa9f
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-08-18 22:25:26 +0200

    Initial add of loadingView

---
+++ GymMealPrep/Model/LoadingView.swift
@@ -0,0 +1,20 @@
+//
+//  LoadingView.swift
+//  GymMealPrep
+//
+//  Created by Tomasz Kubiak on 18/08/2023.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
