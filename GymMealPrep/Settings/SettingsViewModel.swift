6a966d6dcf588e51ac7091c9132e2019c02bbd32
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-10-01 21:42:09 +0200

    Change settings array to use setting sections, groups and models Add private funcs to help with initialisation of array

--- GymMealPrep/Settings/SettingsViewModel.swift
+++ GymMealPrep/Settings/SettingsViewModel.swift
@@ -8,16 +8,47 @@
 import Foundation
 
 class SettingsViewModel: ObservableObject {
-    @Published private var settingStore: SettingStore = .init()
     
-    var setingsArray: [SettingModel]
+    @Published private var settingStore: SettingStore
+    @Published var settings: [SettingSection] = []
     
-    init() {
-        self.setingsArray = .init()
-        self.setingsArray = [
-            SettingModel(setting: .calorieTarget, value: .int(settingStore.calorieTarget)),
-            SettingModel(setting: .macroTargetProtein, value: .int(settingStore.proteinTarget)),
-            SettingModel(setting: .useImperial, value: .bool(settingStore.useImperial))
-        ]
+    init(settingStore: SettingStore = SettingStore()) {
+        self.settingStore = settingStore
+        self.settings = createSettingSections()
+    }
+    
+    private func createSettingSections() -> [SettingSection] {
+        var result = [SettingSection]()
+        let dietSection = SettingSection(
+            sectionName: "Diet",
+            items: [
+                makeSettingModel(setting: .calorieTarget),
+                SettingGroup(
+                    iconSystemName: "chart.pie",
+                    label: "Macro targets",
+                    settings: [
+                        makeSettingModel(setting: .macroTargetProtein),
+                        makeSettingModel(setting: .macroTargetFat),
+                        makeSettingModel(setting: .macroTargetCarb)
+                    ])
+            ])
+        let mealPlanSection = SettingSection(
+            sectionName: "Meal Plan",
+            items: [
+                makeSettingModel(setting: .numberOfMeals),
+                makeSettingModel(setting: .mealNames)
+            ])
+        let variousSetting = SettingSection(sectionName: "Various", items: [
+            makeSettingModel(setting: .units),
+            makeSettingModel(setting: .theme)
+        ])
+        result.append(dietSection)
+        result.append(mealPlanSection)
+        result.append(variousSetting)
+        return result
+    }
+    
+    private func makeSettingModel(setting: Setting) -> SettingModel {
+        return SettingModel(setting: setting, value: settingStore.settings[setting] as Any?)
     }
 }
