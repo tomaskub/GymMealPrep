c4e3f0cbc5db2fd3a1905de20943144e350707eb
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-10-04 22:16:11 +0200

    Add view model for settings detail view

--- GymMealPrep/Settings/SettingsDetailViewModel.swift
+++ GymMealPrep/Settings/SettingsDetailViewModel.swift
@@ -5,6 +5,35 @@
 //  Created by Tomasz Kubiak on 04/10/2023.
 //
 
-import Foundation
+import SwiftUI
+
+class SettingsDetailViewModel: ObservableObject {
+    
+    var settingModels: [SettingModel]
+    @Published private var settingStore: SettingStore
+    @Published var settingValues: [Setting : Any] = .init()
+    
+    init(settingStore: SettingStore, settingModels: [SettingModel]) {
+        self.settingStore = settingStore
+        self.settingModels = settingModels
+        self.settingValues = makeSettingValuesDict(settingModels: settingModels)
+    }
+    
+    private func makeSettingValuesDict(settingModels: [SettingModel]) -> [Setting : Any] {
+        var result = [Setting : Any]()
+        for model in settingModels {
+            result.updateValue(settingStore.settings[model.setting] as Any, forKey: model.setting)
+        }
+        return result
+    }
+    
+    func binding<T>(type: T.Type, for key: Setting) -> Binding<T> where T: Initializable {
+        return Binding {
+            return self.settingValues[key] as? T ?? .init()
+        } set: { value in
+            self.settingValues[key] = value
+        }
+    }
+}
 
 
