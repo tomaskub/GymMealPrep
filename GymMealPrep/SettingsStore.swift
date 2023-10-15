35b0ce886deb558a29a9c241f0be457418b06ce2
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-10-15 14:21:33 +0200

    Extract creating settings dictionary to separate function

--- GymMealPrep/SettingsStore.swift
+++ GymMealPrep/SettingsStore.swift
@@ -18,34 +18,38 @@ class SettingStore: ObservableObject {
     private let defaults = UserDefaults.standard
     private var subscriptions = Set<AnyCancellable>()
 
-    @Published var settings: [Setting : Any?]
+    @Published var settings: [Setting : Any?] = .init()
     
     init() {
-        self.settings = .init()
+        self.settings = createSettings()
+    }
+    
+    private func createSettings() -> [Setting: Any?] {
+        var result: [Setting : Any?] = .init()
         for setting in Setting.allCases {
             switch setting.value {
             case .integer:
-                settings.updateValue(defaults.integer(forKey: setting.key), forKey: setting)
+                result.updateValue(defaults.integer(forKey: setting.key), forKey: setting)
             case .bool:
-                settings.updateValue(defaults.bool(forKey: setting.key), forKey: setting)
+                result.updateValue(defaults.bool(forKey: setting.key), forKey: setting)
             case .string:
-                settings.updateValue(defaults.string(forKey: setting.key), forKey: setting)
+                result.updateValue(defaults.string(forKey: setting.key), forKey: setting)
             case .stringArray:
-                settings.updateValue(defaults.stringArray(forKey: setting.key), forKey: setting)
+                result.updateValue(defaults.stringArray(forKey: setting.key), forKey: setting)
             case .date:
                 if let date = defaults.object(forKey: setting.key) as? Date {
-                    settings.updateValue(date, forKey: setting)
+                    result.updateValue(date, forKey: setting)
                 }
             case .enumeration(let enumType):
                 if let value = defaults.string(forKey: setting.key) {
                     switch enumType {
                     case is Units.Type:
-                        if let result = Units(rawValue: value) {
-                            settings.updateValue(result, forKey: setting)
+                        if let localResult = Units(rawValue: value) {
+                            result.updateValue(localResult, forKey: setting)
                         }
                     case is Theme.Type:
-                        if let result = Theme(rawValue: value) {
-                            settings.updateValue(result, forKey: setting)
+                        if let localResult = Theme(rawValue: value) {
+                            result.updateValue(localResult, forKey: setting)
                         }
                     default:
                         fatalError("Failed at gretting enumeration initialized for setting store dictonary")
@@ -53,9 +57,9 @@ class SettingStore: ObservableObject {
                 } else {
                     switch enumType {
                     case is Units.Type:
-                            settings.updateValue(Units.defaultValue, forKey: setting)
+                            result.updateValue(Units.defaultValue, forKey: setting)
                     case is Theme.Type:
-                            settings.updateValue(Theme.defaultValue, forKey: setting)
+                            result.updateValue(Theme.defaultValue, forKey: setting)
                     default:
                         fatalError("Failed at gretting enumeration initialized for setting store dictonary")
                     }
@@ -63,8 +67,8 @@ class SettingStore: ObservableObject {
             case .nilValue:
                 break
             }
-            
         }
+        return result
     }
 }
 extension SettingStore {
