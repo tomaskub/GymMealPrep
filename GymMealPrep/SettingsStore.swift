860a600520a4b6002208cf75da55b7d17bb2779f
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-10-15 14:16:49 +0200

    Remove unused published values and associated combine subs

--- GymMealPrep/SettingsStore.swift
+++ GymMealPrep/SettingsStore.swift
@@ -10,29 +10,17 @@ import Combine
 
 class SettingStore: ObservableObject {
     
-    let dateFormatter = {
+    private let dateFormatter = {
        let result = DateFormatter()
         result.dateFormat = "dd/MM/yyy HH:mm"
         return result
     }()
     private let defaults = UserDefaults.standard
     private var subscriptions = Set<AnyCancellable>()
-    
-    @Published var calorieTarget: Int
-    @Published var proteinTarget: Int
-    @Published var fatTarget: Int
-    @Published var carbTarget: Int
-    @Published var units: String
-    
+
     @Published var settings: [Setting : Any?]
     
     init() {
-        self.calorieTarget = defaults.integer(forKey: Setting.calorieTarget.key)
-        self.proteinTarget = defaults.integer(forKey: Setting.macroTargetProtein.key)
-        self.fatTarget = defaults.integer(forKey: Setting.macroTargetFat.key)
-        self.carbTarget = defaults.integer(forKey: Setting.macroTargetCarb.key)
-        self.units = defaults.string(forKey: Setting.units.key) ?? "metric"
-        
         self.settings = .init()
         for setting in Setting.allCases {
             switch setting.value {
@@ -77,28 +65,9 @@ class SettingStore: ObservableObject {
             }
             
         }
-        self.setUpWriteSubscribers()
     }
 }
 extension SettingStore {
-    private func setUpWriteSubscribers() {
-        
-        $calorieTarget.sink { [weak self] calories in
-            self?.updateSetting(for: .calorieTarget, with: calories)
-        }.store(in: &subscriptions)
-        
-        $proteinTarget.sink { [weak self] proteins in
-            self?.updateSetting(for: .macroTargetProtein, with: proteins)
-        }.store(in: &subscriptions)
-        
-        $fatTarget.sink { [weak self] fats in
-            self?.updateSetting(for: .macroTargetFat, with: fats)
-        }.store(in: &subscriptions)
-        
-        $carbTarget.sink { [weak self] carbs in
-            self?.updateSetting(for: .macroTargetCarb, with: carbs)
-        }.store(in: &subscriptions)
-    }
     private func updateSetting(for setting: Setting, with value: Any?) {
         defaults.set(value, forKey: setting.key)
     }
