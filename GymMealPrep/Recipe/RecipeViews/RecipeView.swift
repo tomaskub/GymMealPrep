c21af412f3f57c7ecaea7eb224b231ecada239d9
Tomasz Kubiak <tomasz.m.kubiak@icloud.com>
2023-10-30 17:32:27 +0100

    Remove default values for init properties and implement container in previews

--- GymMealPrep/Recipe/RecipeViews/RecipeView.swift
+++ GymMealPrep/Recipe/RecipeViews/RecipeView.swift
@@ -18,51 +18,49 @@ struct RecipeView: View {
                 .ignoresSafeArea()
             
             //Content layer
-                List {
-
-                    Section {
-                        titleBanner
-                            .listRowInsets(EdgeInsets())
-                        tagChipView
-                            .listRowInsets(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
-                        HStack {
-                            Spacer()
-                            RecipeSummaryView(timePreparingInMinutes: viewModel.recipe.timePreparingInMinutes ?? 0,
-                                              timeCookingInMinutes: viewModel.recipe.timeCookingInMinutes ?? 0,
-                                              timeWaitingInMinues: viewModel.recipe.timeWaitingInMinutes ?? 0,
-                                              timeTotalInMinutes: viewModel.totalTimeCookingInMinutes,
-                                              cal: viewModel.recipe.nutritionData.calories,
-                                              proteinInGrams: viewModel.recipe.nutritionData.protein,
-                                              fatInGrams: viewModel.recipe.nutritionData.fat,
-                                              carbInGrams: viewModel.recipe.nutritionData.carb,
-                                              servings: viewModel.recipe.servings,
-                                              format: "%.0f")
-                            Spacer()
-                        }
-                    }
-                    .listRowSeparator(.hidden)
-                    
-                    
-                    Section("Ingredients:") {
-                        ForEach(viewModel.recipe.ingredients) {
-                            i in
-                            HStack {
-                                Text(i.food.name)
-                                Spacer()
-                                Text(String(format: "%.2f",i.quantity))
-                                Text(i.unitOfMeasure)
-                            }
-                            
-                        }
-                    }
-                    Section("Instructions") {
-                        ForEach($viewModel.recipe.instructions) { instruction in
-                            InstructionRowView(instructionText: instruction.text, step: instruction.step.wrappedValue, editable: false)
-                        }
+            List {
+                Section {
+                    titleBanner
+                        .listRowInsets(EdgeInsets())
+                    tagChipView
+                        .listRowInsets(EdgeInsets(top: 8, leading: 8, bottom: 8, trailing: 8))
+                    HStack {
+                        Spacer()
+                        RecipeSummaryView(timePreparingInMinutes: viewModel.recipe.timePreparingInMinutes ?? 0,
+                                          timeCookingInMinutes: viewModel.recipe.timeCookingInMinutes ?? 0,
+                                          timeWaitingInMinues: viewModel.recipe.timeWaitingInMinutes ?? 0,
+                                          timeTotalInMinutes: viewModel.totalTimeCookingInMinutes,
+                                          cal: viewModel.recipe.nutritionData.calories,
+                                          proteinInGrams: viewModel.recipe.nutritionData.protein,
+                                          fatInGrams: viewModel.recipe.nutritionData.fat,
+                                          carbInGrams: viewModel.recipe.nutritionData.carb,
+                                          servings: viewModel.recipe.servings,
+                                          format: "%.0f")
+                        Spacer()
                     }
                 }
-                .listStyle(.inset)
-                .ignoresSafeArea(edges: .top)
+                .listRowSeparator(.hidden)
+                
+                Section("Ingredients:") {
+                    ForEach(viewModel.recipe.ingredients) {
+                        i in
+                        HStack {
+                            Text(i.food.name)
+                            Spacer()
+                            Text(String(format: "%.2f",i.quantity))
+                            Text(i.unitOfMeasure)
+                        }
+                        
+                    }
+                }
+                Section("Instructions") {
+                    ForEach($viewModel.recipe.instructions) { instruction in
+                        InstructionRowView(instructionText: instruction.text, step: instruction.step.wrappedValue, editable: false)
+                    }
+                }
+            }
+            .listStyle(.inset)
+            .ignoresSafeArea(edges: .top)
         }
     }
     
@@ -90,25 +88,32 @@ struct RecipeView: View {
     }
     
     var tagChipView: some View {
-        
-        
         ChipView(tags: $viewModel.recipe.tags, avaliableWidth: UIScreen.main.bounds.width - 20, alignment: .leading) { tag in
-                    Text(tag.text)
-                        .foregroundColor(.white)
-                        .padding(.horizontal)
-                        .padding(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 0))
-                        .background(
-                            Capsule()
-                            .foregroundColor(.blue))
-                }
-         
+            Text(tag.text)
+                .foregroundColor(.white)
+                .padding(.horizontal)
+                .padding(EdgeInsets(top: 4, leading: 0, bottom: 4, trailing: 0))
+                .background(
+                    Capsule()
+                        .foregroundColor(.blue))
+        }
     }
 }
 
 struct RecipieView_Previews: PreviewProvider {
-    static var previews: some View {
-        NavigationView {
-            RecipeView(viewModel: RecipeViewModel(recipe: SampleData.recipieCilantroLimeChicken, dataManager: DataManager.preview))
+    private struct PreviewContainerView: View {
+        @StateObject private var container = Container()
+        var body: some View {
+            NavigationView {
+                RecipeView(viewModel: 
+                            RecipeViewModel(recipe: SampleData.recipieCilantroLimeChicken,
+                                            dataManager: container.dataManager))
+            }
+            .environmentObject(container)
         }
+    }
+    
+    static var previews: some View {
+        PreviewContainerView()
     }
 }
